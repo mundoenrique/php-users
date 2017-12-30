4ba19b0f9952d4418967f78242324e2f611d70bc
Miguel Palazzo <mpalazzo@novopayment.com>
2017-12-30 19:42:34 -0400

    Remueve aplicación mantenida con CodeIgniter 2

--- application/language/ve-spanish/admin_lang.php
+++
@@ -1,5 +0,0 @@
-<?php
-$lang['titleHeading'] = "EOL VENEZUELA";
-$lang['login'] = "Login";
-$lang['password'] = "Contraseña";
-$lang['loginBtn'] = "Entrar";
--- application/views/widgets/widget-menuFooter.php
+++
@@ -1,8 +0,0 @@
-	
-	<nav id="extra-nav">
-		<ul class="menu">
-			<li class="menu-item terms">
-				<a href="<? echo $this->config->item("base_url"); ?>/users/terminosCondiciones" rel="section">Condiciones de uso</a>
-			</li>
-		</ul>
-	</nav>
