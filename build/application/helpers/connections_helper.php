52e38b10105fcf4928c9bae0503f72c3a818c48d
Enrique Peñaloza <epenaloza@novopayment.com>
2018-06-05 10:25:12 -0400

    Incorpora timeout en la petición al servicio

--- build/application/helpers/connections_helper.php
+++ build/application/helpers/connections_helper.php
@@ -22,6 +22,7 @@ if ( ! function_exists('np_Hoplite_GetWS'))
 		curl_setopt($ch, CURLOPT_URL, $urlcurlWS);
 		curl_setopt($ch, CURLOPT_POST, TRUE);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 60);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $dataPost);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 		    'Content-Type: text/plain',
@@ -29,10 +30,13 @@ if ( ! function_exists('np_Hoplite_GetWS'))
 		);
 		$response = curl_exec($ch);
 		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		log_message("ERROR","CURL HTTP CODE: " . $httpCode);
-		if($httpCode==404){
-			return FALSE;
-		}else{
+		if(curl_exec($ch) === FALSE) {
+			log_message("DEBUG","RESPONSE CURL TIMEOUT: " .json_encode(curl_error($ch)));
+		}
+		log_message("DEBUG","RESPONSE CURL HTTP CODE: ".$httpCode);
+		if($httpCode == 404 || curl_exec($ch) === FALSE){
+			return '{"data": false}';
+		} else {
 			return $response;
 		}
 
