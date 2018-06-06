580c0874c8977c412dedb21d487214ab07081437
Enrique Peñaloza <epenaloza@novopayment.com>
2018-06-06 15:20:41 -0400

    Quita timeout porque genera peticiones recurrentes

--- build/application/helpers/connections_helper.php
+++ build/application/helpers/connections_helper.php
@@ -22,17 +22,13 @@ if ( ! function_exists('np_Hoplite_GetWS'))
 		curl_setopt($ch, CURLOPT_URL, $urlcurlWS);
 		curl_setopt($ch, CURLOPT_POST, TRUE);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-		curl_setopt($ch, CURLOPT_TIMEOUT, 60);
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $dataPost);
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-		    'Content-Type: text/plain',
-		    'Content-Length: ' . strlen($dataPost))
+			'Content-Type: text/plain',
+			'Content-Length: ' . strlen($dataPost))
 		);
 		$response = curl_exec($ch);
 		$httpCode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
-		if(curl_exec($ch) === FALSE) {
-			log_message("DEBUG","RESPONSE CURL TIMEOUT: " .json_encode(curl_error($ch)));
-		}
 		log_message("DEBUG","RESPONSE CURL HTTP CODE: ".$httpCode);
 		if($httpCode == 404 || curl_exec($ch) === FALSE){
 			return '{"data": false}';
