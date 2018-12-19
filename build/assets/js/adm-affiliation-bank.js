61bee0d46f10018024f254bd3bfcff44ca033601
Enrique Peñaloza <epenaloza@novopayment.com>
2018-12-19 11:11:49 -0400

    Extiende rango de fecha de vencimiento hasta 15 años para ctas origen

--- build/assets/js/adm-affiliation-bank.js
+++ build/assets/js/adm-affiliation-bank.js
@@ -199,7 +199,7 @@ $(function(){
 					yearNow = new Date();
 					fullYearDate = yearNow.getFullYear();
 					fiveyearLess = fullYearDate - 5;
-					fiveYearMore = fullYearDate + 10;
+					fiveYearMore = fullYearDate + 15;
 
 					for (i = fiveyearLess; i <= fiveYearMore; i++){
 						yearSelect.push(i);
