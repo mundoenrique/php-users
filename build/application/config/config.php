df6f5e5123896ac905d8cdc4de644e0f934ff1f7
jhllerena <jhllerena@novopayment.com>
2019-02-13 12:23:25 -0400

    Incorpora variables url API y credenciales oauth

--- build/application/config/config.php
+++ build/application/config/config.php
@@ -28,6 +28,11 @@ $config['base_url_cdn'] = BASE_CDN_URL;
 $config['base_path_cdn'] = BASE_CDN_PATH;
 $config['urlWS'] = WS_URL;
 $config['keyNovo'] = WS_KEY;
+//url API
+$config['urlAPI'] = URL_API;
+//Credenciales oauth billPayments
+$config['clientId_billPayment'] = CLIENT_ID;
+$config['clientSecret_billPaymet'] = CLIENT_SECRET;
 
 /*
 |--------------------------------------------------------------------------
