aeae466cedb15613c36860aecb1902629a2b74dc
Jarvis Rangel <jarangel@novopayment.com>
2019-03-23 16:49:34 -0400

    Incorpora favicon dependiendo del skin

--- build/application/views/layouts/layout-header.php
+++ build/application/views/layouts/layout-header.php
@@ -1,3 +1,11 @@
+<?php
+$CI =& get_instance();
+$pageClass = isset($bodyclass) ? 'class="' . $bodyclass . '"' : '';
+$pageUrl = $CI->config->item('base_url');
+$pageCdn = $CI->config->item('base_url_cdn');
+$skin = $CI->input->cookie($CI->config->item('cookie_prefix') . 'skin');
+$extension = $skin != "pichincha" ? ".png": ".ico";
+?>
 <!DOCTYPE html>
 <html class="no-js" lang="es">
 <head>
@@ -6,6 +14,7 @@
     <meta name="viewport" content="width=device-width" />
     <meta name="robots" content="noindex, nofollow" />
     <meta name="googlebot" content="none" />
+		<link rel="icon" type="image/png"  href="<?php echo $pageCdn;?>img/favicon<?php echo $extension;?>" />
     <?php
 			$cookie = $this->input->cookie($this->config->item('cookie_prefix').'skin');
 			if($this->router->class != 'service' && $cookie == 'pichincha') {
@@ -18,13 +27,6 @@
 			echo insert_js_cdn('html5.js');
 		?>
 </head>
-<?php
-$CI =& get_instance();
-$pageClass = isset($bodyclass) ? 'class="' . $bodyclass . '"' : '';
-$pageUrl = $CI->config->item('base_url');
-$pageCdn = $CI->config->item('base_url_cdn');
-$skin = $CI->input->cookie($CI->config->item('cookie_prefix') . 'skin');
-?>
 <body <?php echo $pageClass;?> data-app-url="<?php echo $pageUrl;?>" data-app-cdn="<?php echo $pageCdn;?>" data-country="<?php echo $this->session->userdata('pais') ?>">
 <header id="head">
 
