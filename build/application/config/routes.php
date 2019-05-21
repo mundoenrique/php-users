425f1be7cc9a870d38cedf22aabc2285d797a041
Yelsyns lopez <ylopez@novopayment.com>
2019-05-21 13:52:53 -0500

    Incorpora recaptcha V3 en login

--- build/application/config/routes.php
+++ build/application/config/routes.php
@@ -63,6 +63,7 @@ $route['users/obtenerLogin_pe'] = 'users/obtenerLogin'; // Perú LATODO
 $route['users/obtenerLogin_pi'] = 'users/obtenerLogin'; // Ecuador pichincha
 $route['users/obtenerLogin'] = 'users/obtenerLogin'; // Otros paises
 $route['users/login'] = 'users/CallWsLogin';
+$route['users/validateRecaptcha'] = 'users/validarCaptcha';
 $route['users/error'] = 'users/error_gral';
 $route['users/resetpassword'] = 'users/CallWsResetPassword';
 $route['users/obtenerlogin_call'] = 'users/CallWsObtenerLogin';
