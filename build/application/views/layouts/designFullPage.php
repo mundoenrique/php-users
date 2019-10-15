0ee593f49e6a7b9de7808afd204692c09862ac93
Jhonathan Llerena <jhllerena@novopayment.com>
2019-10-15 12:36:11 -0400

    Actualiza footer y header

--- build/application/views/layouts/designFullPage.php
+++ build/application/views/layouts/designFullPage.php
@@ -17,7 +17,7 @@
 	?>
 		<header class="main-head">
 			<div class="flex">
-				<img src="<?= $this->asset->insertFile('img-logo.svg','img',$countryUri); ?>" alt="" />
+				<img src="<?= $this->asset->insertFile('img-logo.svg','img',$countryUri); ?>" alt="Logo Banco de Bogotá"/>
 			</div>
 		</header>
 	<?php
@@ -33,16 +33,14 @@
   </article>
 
 	<footer class="main-footer">
-		<div class="flex mr-4">
+		<div class="flex pr-2 pr-lg-0">
 			<img class="" src="<?= $this->asset->insertFile('img-mark.svg','img',$countryUri); ?>" alt="Logo Superintendencia">
 		</div>
-    <div class="flex flex-auto flex-wrap justify-between items-center">
+		<div class="flex flex-auto flex-wrap justify-around items-center">
 			<img class="order-first" src="<?= $this->asset->insertFile('img-bogota_white.svg','img',$countryUri); ?>" alt="Logo Banco de Bogotá">
 			<img class="order-1" src="<?= $this->asset->insertFile('img-pci_compliance.svg','img',$countryUri); ?>" alt="Logo PCI">
-			<img class="order-1" src="<?= $this->asset->insertFile('img-engine.svg','img',$countryUri); ?>assets/images/img-engine.svg" alt="Logo NovoPayment">
-
-      <span class="copyright-footer mt-1 nowrap flex-auto lg-flex-none order-1 order-lg-0 center h6">© Todos los derechos reservados. Banco de Bogotá - 2019.</span>
-    </div>
+			<span class="copyright-footer mt-1 nowrap flex-auto lg-flex-none order-1 order-lg-0 center h6">© Todos los derechos reservados. Banco de Bogotá - 2019.</span>
+		</div>
 	</footer>
 
 	<?php
