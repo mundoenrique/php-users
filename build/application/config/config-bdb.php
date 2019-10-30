05c673c7283de9d19877dedf111d0a18b7bdebfb
Pedro Torres <ptorres@novopayment.com>
2019-10-30 09:51:50 -0400

    corrige comportamiento de slash desde el config

--- build/application/config/config-bdb.php
+++ build/application/config/config-bdb.php
@@ -1,10 +1,10 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 
 //País
-$config['country']='bdb';
+$config['country'] = 'bdb';
 $config['language'] = 'spanish-bdb';
-$config['base_url']	= BASE_URL.'bdb/';
+$config['base_url']	= BASE_URL . '/' . 'bdb/';
 $config['language_file_specific'] = [];
 $config['language_form_validate'] = FALSE;
 $config['layout'] = 'designFullPage';
