d169ec4f1a79edb00f9bf8dc19cb2fb77c66019a
Jhonathan Llerena <jhllerena@novopayment.com>
2019-11-14 19:50:38 -0400

    Incorpora lista movimientos

--- build/application/views/product/detailproduct_content.php
+++ build/application/views/product/detailproduct_content.php
@@ -11,10 +11,10 @@
 						<img class="card-image" src="<?= $this->asset->insertFile('img-card_gray.svg','img',$countryUri); ?>" alt="Tarjeta gris">
 					</div>
 					<div class="product-info-full" moneda="Bs.">
-						<p class="product-cardholder mb-1 semibold h4 primary"><?= $data['nom_plastico']; ?></p>
+						<p class="product-cardholder mb-1 semibold h4 primary capitalize"><?= $data['nom_plastico']; ?></p>
 						<p id="card" class="product-cardnumber mb-0 primary"><?= $data['noTarjetaConMascara'];?></p>
-						<p class="product-metadata mb-2 h6"><?= $data['nombre_producto'];?></p>
-						<ul class="product-balance-group list-inline">
+						<p class="product-metadata mb-2 h6 capitalize"><?= $data['nombre_producto'];?></p>
+						<ul class="product-balance-group flex justify-between mb-0 list-inline">
 							<li class="list-inline-item">Actual <span id="actual" class="product-balance block primary"><?= $data['actualBalance'];?></span></li>
 							<li class="list-inline-item">En Tránsito <span id="bloqueado" class="product-balance block primary"><?= $data['ledgerBalance'];?></span></li>
 							<li class="list-inline-item">Disponible <span id="disponible" class="product-balance block primary"><?= $data['availableBalance'];?></span></li>
@@ -74,10 +74,48 @@
 					</div>
 				</nav>
 
-				<div class="line mt-1"></div>
-
-
+				<div class="group row mt-3" id="results">
+					<div class="group-main-view col-8" id="transactions">
+						<h3 class="h4 regular">Actividad <span id="period">reciente</span>
+							<span id="transit-datail-title" style="display: none;">Transacciones Pendientes</span>
+						</h3>
+						<div class="line mt-1"></div>
+						<ul id="list-detail" class="feed list-style-none mt-3 pl-0">
+							<li class="feed-item feed-income flex py-1 items-center">
+								<div class="flex px-2 border-right flex-column items-center feed-date">
+									<span class="h5 feed-date-day">08</span>
+									<span class="h6 uppercase feed-date-month">Nov</span>
+									<span class="h6 feed-date-year">2019</span>
+								</div>
+								<div class="flex px-2 flex-column mr-auto">
+									<span class="h5 uppercase semibold feed-product">BENEFICIO DE ALIMENTACION</span>
+									<span class="h6 feed-metadata">364271</span>
+								</div>
+								<span class="px-2 feed-amount items-center">Bs. 325.000,00</span>
+							</li>
+							<li class="feed-item feed-expense flex py-1 items-center">
+								<div class="flex px-2 border-right flex-column items-center feed-date">
+									<span class="h5 feed-date-day">08</span>
+									<span class="h6 uppercase feed-date-month">Nov</span>
+									<span class="h6 feed-date-year">2019</span>
+								</div>
+								<div class="flex px-2 flex-column mr-auto">
+									<span class="h5 uppercase semibold feed-product">BENEFICIO DE ALIMENTACION</span>
+									<span class="h6 feed-metadata">364271</span>
+								</div>
+								<span class="px-2 feed-amount items-center">Bs. -325.000,00</span>
+							</li>
+						</ul>
+					</div>
+					<div class="group-aside-view col-4" id="stats">
+						<h3 class="h4 regular">Estadísticas</h3>
+						<div class="line mt-1"></div>
+						<div id="estadisticas" style="width: 300px; height: 250px; position: relative;" data-role="chart" class=" k-chart"><svg style="width: 100%; height: 100%; overflow: hidden; left: -0.5px; top: -0.5px;" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><defs></defs><g><path d="M0 0 L 300 0 300 250 0 250Z" stroke="none" fill="transparent"></path><path d="M0 0 L 0 0 0 0 0 0Z" stroke="none" fill="transparent" fill-opacity="0"></path><g><g></g></g><g><g><g transform="matrix(1,0,0,1,0,0)"><path d="M150 41 C 169.394 41 188.715 47.972 203.64 60.357 218.565 72.741 228.982 90.445 232.559 109.507 236.136 128.568 232.847 148.844 223.428 165.797 L 181.469 142.484 C 185.506 135.219 186.915 126.529 185.382 118.36 183.849 110.191 179.385 102.603 172.989 97.296 166.592 91.988 158.312 89 150 89Z" stroke="none" fill="#E74C3C"></path></g><g transform="matrix(1,0,0,1,0,0)"><path d="M223.428 165.797 C 213.896 182.951 198.157 196.568 179.81 203.533 161.463 210.497 140.652 210.754 122.138 204.245 103.625 197.736 87.554 184.511 77.602 167.597 67.65 150.683 63.896 130.212 67.197 110.868 70.499 91.523 80.831 73.456 95.829 60.801 110.828 48.145 130.375 41 150 41 L 150 89 C 141.589 89 133.212 92.062 126.784 97.486 120.356 102.91 115.928 110.653 114.513 118.943 113.098 127.234 114.707 136.007 118.972 143.256 123.237 150.505 130.125 156.172 138.059 158.962 145.994 161.752 154.913 161.642 162.776 158.657 170.639 155.672 177.384 149.836 181.469 142.484Z" stroke="none" fill="#2ECC71"></path></g></g></g></g></svg></div>
+						<div id="estadisticas-transit" style="width: 300px; height: 250px; visibility: hidden; display: none;"></div>
+					</div>
 				</div>
+
+				<div class="line mt-1"></div>
 			</div>
 		</section>
 	</div>
