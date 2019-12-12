dae23bc975fa7ef01954ba1beb5637c6b76edd92
Jhonathan Llerena <jhllerena@novopayment.com>
2019-12-12 16:47:18 -0400

    Incorpora validaciones en formulario de perfil

--- build/assets/js/bdb/user/profile.js
+++ build/assets/js/bdb/user/profile.js
@@ -4,13 +4,30 @@ var $$ = document;
 $$.addEventListener('DOMContentLoaded', function(){
 	//vars
 	var maxBirthdayDate = new Date();
-  maxBirthdayDate.setFullYear( maxBirthdayDate.getFullYear() - 18);
+	maxBirthdayDate.setFullYear( maxBirthdayDate.getFullYear() - 18);
+	var btnTrigger = $$.getElementById('btnActualizar');
+	var txtBtnTrigger = btnTrigger.innerHTML.trim();
 
 	//core
 	$( "#birthDate" ).datepicker( {
 		maxDate: maxBirthdayDate,
     yearRange: "-99:"+maxBirthdayDate,
 		defaultDate: "-30y"
-  });
+	});
+
+	btnTrigger.addEventListener('click', function(){
+
+
+		var form = $('#formProfile');
+		validateForms(form, {handleMsg: true});
+		if(form.valid()) {
+			btnTriggerOTP.disabled = true;
+			console.log("todo bien");
+
+		} else {
+			console.log("campos no válidos");
+		}
+
+	});
 
 })
