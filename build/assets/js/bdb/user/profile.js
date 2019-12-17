e77d7a01428437edeeae2cf6bb318b504a857be2
Jhonathan Llerena <jhllerena@novopayment.com>
2019-12-17 16:49:20 -0400

    Incorpora listado de ciudades asíncrono

--- build/assets/js/bdb/user/profile.js
+++ build/assets/js/bdb/user/profile.js
@@ -6,11 +6,16 @@ $$.addEventListener('DOMContentLoaded', function(){
 	var maxBirthdayDate = new Date();
 	var btnTrigger = $$.getElementById('btnActualizar');
 	var listStates = $$.getElementById('department');
-	var listCity = $$.getElementById('ctrlCity');
+	var listCity = $$.getElementById('city');
 	var txtBtnTrigger = btnTrigger.innerHTML.trim();
+	var loadingOption = createElement('option', {value: ''});
+	var selectOption = createElement('option', {value: ''});
 
-	maxBirthdayDate.setFullYear( maxBirthdayDate.getFullYear() - 18);
 	//core
+	maxBirthdayDate.setFullYear( maxBirthdayDate.getFullYear() - 18);
+	loadingOption.textContent = 'Cargando...';
+	selectOption.textContent = 'Seleccione';
+
 	$( "#birthDate" ).datepicker( {
 		maxDate: maxBirthdayDate,
     yearRange: "-99:"+maxBirthdayDate,
@@ -34,16 +39,45 @@ $$.addEventListener('DOMContentLoaded', function(){
 
 	listStates.addEventListener('change', function(){
 		if (this.value !== '') {
-			listCity.classList.remove('none');
+			// listCity.classList.remove('none');
+			while (listCity.firstChild) {
+				listCity.removeChild(listCity.firstChild);
+			}
+			listCity.appendChild(loadingOption);
 			data = {
 				codState: this.value,
 			}
 
 			callNovoCore('POST', 'User', 'getListCitys', data, function(response) {
-				console.log(response.data);
+
+				listCity.removeChild(listCity.firstChild);
+				if (response.code === 0) {
+					listCity.appendChild(selectOption);
+					response.data.forEach(function callback(currentValue, index, array) {
+						var city = createElement('option', {value: currentValue.codCiudad});
+						city.textContent = currentValue.ciudad;
+						listCity.appendChild(city);
+					});
+					listCity.disabled = false;
+				} else {
+					var noResponse = createElement('option', {value: ''});
+					noResponse.textContent = response.data.descripcion;
+					listCity.appendChild(noResponse);
+				}
 			});
 		}else{
-			listCity.classList.add('none');
 		}
 	});
 })
+
+var createElement = function (tagName, attrs) {
+
+	var el = document.createElement(tagName);
+	Object.keys(attrs).forEach((key) => {
+		if (attrs [key] !== undefined) {
+			el.setAttribute(key, attrs [key]);
+		}
+	});
+
+	return el;
+}
