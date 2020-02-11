8e17d1adf54776e478216caa97e6f8e6c34a0e66
Pedro Torres <ptorres@novopayment.com>
2020-02-11 11:24:35 -0400

    Controla respuesta del tiempo de validades para cod OTP

--- build/application/models/Novo_ServiceProduct_Model.php
+++ build/application/models/Novo_ServiceProduct_Model.php
@@ -12,6 +12,7 @@ class Novo_ServiceProduct_Model extends NOVO_Model
 	{
 		parent::__construct();
 		log_message('INFO', 'NOVO User Model Class Initialized');
+		$this->defaultTimeOTP = 2;
 	}
 
 	public function callWs_generate_ServiceProduct($dataRequest)
@@ -63,7 +64,7 @@ class Novo_ServiceProduct_Model extends NOVO_Model
 				break;
 				case 10:
 					$this->response->code = 1;
-					$this->response->validityTime = intval($response->bean) * 60;
+					$this->response->validityTime = intval($response->bean) !== 0 ? intval($response->bean) * 60: $this->defaultTimeOTP * 60;
 					$this->response->msg = lang('RESP_PIN_EXPIRED');
 					break;
 				case -308:
@@ -226,6 +227,7 @@ class Novo_ServiceProduct_Model extends NOVO_Model
 				case 10:
 					$this->response->code = 1;
 					$this->response->msg = lang('RESP_CODEOTP');
+					$this->response->validityTime = intval($response->bean) !== 0 ? intval($response->bean) * 60: $this->defaultTimeOTP * 60;
 					break;
 				case -308:
 					$this->response->code = 2;
@@ -385,7 +387,8 @@ class Novo_ServiceProduct_Model extends NOVO_Model
 				break;
 				case 10:
 					$this->response->code = 1;
-					break;
+					$this->response->msg = lang('RESP_CODEOTP');
+					$this->response->validityTime = intval($response->bean) !== 0 ? intval($response->bean) * 60: $this->defaultTimeOTP * 60;
 				case -308:
 					$this->response->code = 3;
 					$this->response->msg = lang('RESP_PIN_NOT_VALID');
@@ -545,7 +548,8 @@ class Novo_ServiceProduct_Model extends NOVO_Model
 				break;
 				case 10:
 					$this->response->code = 1;
-					break;
+					$this->response->msg = lang('RESP_CODEOTP');
+					$this->response->validityTime = intval($response->bean) !== 0 ? intval($response->bean) * 60: $this->defaultTimeOTP * 60;
 				case -308:
 					$this->response->code = 3;
 					$this->response->msg = lang('RESP_PIN_NOT_VALID');
