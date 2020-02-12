296044bb0cdeb57080f86ac7d38c4da45e31ffd1
Jhonathan Llerena <jhllerena@novopayment.com>
2020-02-12 12:42:26 -0400

    Corrige textos en vista cambio de contraseña

--- build/application/language/spanish-base/password-change_lang.php
+++ build/application/language/spanish-base/password-change_lang.php
@@ -1,16 +1,17 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 
-$lang['CURRENT_PASSWORD'] = 'Contraseña actual *';
-$lang['NEW_PASSWORD'] = 'Contraseña nueva *';
-$lang['CONFIRM_PASSWORD'] = 'Confirme la nueva contraseña *';
-$lang['PASSWORD_INFO_TITLE'] = 'Requerimientos para configurar la contraseña. La clave debe tener:';
-$lang['PASSWORD_INFO_1'] = 'De 8 a 15 <strong>Caracteres</strong>';
-$lang['PASSWORD_INFO_2'] = 'Al menos una <strong>letra minúscula</strong>';
-$lang['PASSWORD_INFO_3'] = 'Al menos una <strong>letra mayúscula</strong>';
-$lang['PASSWORD_INFO_4'] = 'De 1 a 3 <strong>números</strong>';
-$lang['PASSWORD_INFO_5'] = 'Al menos un <strong>caracter especial</strong> (ej: ! @ * - ? ¡ ¿ + / . , _ #)';
-$lang['PASSWORD_INFO_6'] = 'No debe tener más de 2 <strong>caracteres</strong> iguales consecutivos';
+$lang['DESCRIPTION_MSG'] = 'Por favor complete la información requerida a continuación para generar una nueva contraseña.';
+$lang['CURRENT_PASSWORD'] = 'Contraseña actual <span class="danger">*</span>';
+$lang['NEW_PASSWORD'] = 'Nueva contraseña <span class="danger">*</span>';
+$lang['CONFIRM_PASSWORD'] = 'Confirmar contraseña <span class="danger">*</span>';
+$lang['PASSWORD_INFO_TITLE'] = 'Requerimientos de contraseña:';
+$lang['PASSWORD_INFO_1'] = 'De 8 a 15 <strong>caracteres.</strong>';
+$lang['PASSWORD_INFO_2'] = 'Al menos una <strong>letra minúscula.</strong>';
+$lang['PASSWORD_INFO_3'] = 'Al menos una <strong>letra mayúscula.</strong>';
+$lang['PASSWORD_INFO_4'] = 'De 1 a 3 <strong>números.</strong>';
+$lang['PASSWORD_INFO_5'] = 'Al menos un <strong>carácter especial </strong>(ej: ! @ * - ? ¡ ¿ + / . , _ #).';
+$lang['PASSWORD_INFO_6'] = 'No debe tener más de 2 <strong>caracteres</strong> iguales consecutivos.';
 $lang['MSG_NEW_PASS_USER'] = 'Por motivos de seguridad es necesario que cambie su contraseña la primera vez que ingresa a nuestro sistema "Conexión Empresas Online".';
 $lang['MSG_NEW_PASS_CADU'] = 'Clave vencida. Por motivos de seguridad es necesario que cambies tu contraseña antes de continuar en nuestro sistema "Conexión Empresas Online".';
 $lang['INFO_CHANGE_PASS'] = "Cambio de contraseña";
--- build/application/views/user/changepassword_content.php
+++ build/application/views/user/changepassword_content.php
@@ -2,80 +2,73 @@
 <div id="changepassword" class="changepassword-content h-content bg-white">
 	<div class="py-4 px-5">
 		<header class="">
-			<h1 class="primary h0">Cambiar clave</h1>
+			<h1 class="primary h0"><?= $reason?:'Cambiar contraseña'; ?></h1>
 		</header>
 		<section>
 			<hr class="separador-one">
 			<div class="pt-3">
-				<h2 class="tertiary h3">Verificación de datos</h2>
-				<p><?= $reason?:''; ?></p>
-				<hr class="separador-one">
-				<div class="pt-3">
-					<span>Los campos con  <span class="danger">*</span> son requeridos.</span>
-					<form id="formChangePassword" class="mt-2" method="post">
-						<div class="row">
-							<div class="col-6 col-lg-8 col-xl-6">
-								<h3 class="tertiary h4">Datos de usuario</h3>
-								<div class="row">
-									<div class="form-group col-12 col-lg-6">
-										<label for="currentPassword">Clave actual <span class="danger">*</span></label>
-										<div class="input-group">
-											<input id="currentPassword" class="form-control" type="password" name="currentPassword">
-											<div class="input-group-append">
-												<span id="pwdAddon" class="input-group-text" title="Clic aquí para mostrar/ocultar contraseña"><i class="icon-view mr-0"></i></span>
-											</div>
+				<p><?= lang('DESCRIPTION_MSG'); ?></p>
+				<span>Los campos con  <span class="danger">*</span> son requeridos.</span>
+				<form id="formChangePassword" class="mt-2" method="post">
+					<div class="row">
+						<div class="col-6 col-lg-8 col-xl-6">
+							<div class="row">
+								<div class="form-group col-12 col-lg-6">
+									<label for="currentPassword"><?= lang('CURRENT_PASSWORD'); ?></label>
+									<div class="input-group">
+										<input id="currentPassword" class="form-control" type="password" name="currentPassword">
+										<div class="input-group-append">
+											<span id="pwdAddon" class="input-group-text" title="Clic aquí para mostrar/ocultar contraseña"><i class="icon-view mr-0"></i></span>
 										</div>
-										<div class="help-block"></div>
 									</div>
-								</div>
-								<div class="row">
-									<div class="form-group col-12 col-lg-6">
-										<label for="newPassword">Contraseña <span class="danger">*</span></label>
-										<div class="input-group">
-											<input id="newPassword" class="form-control" type="password" name="newPassword">
-											<div class="input-group-append">
-												<span id="pwdAddon2" class="input-group-text" title="Clic aquí para mostrar/ocultar contraseña"><i class="icon-view mr-0"></i></span>
-											</div>
-										</div>
-										<div class="help-block"></div>
-									</div>
-									<div class="form-group col-12 col-lg-6">
-										<label for="confirmPassword">Confirmar contraseña <span class="danger">*</span></label>
-										<div class="input-group">
-											<input id="confirmPassword" class="form-control" type="password" name="confirmPassword">
-											<div class="input-group-append">
-												<span id="pwdAddon3" class="input-group-text" title="Clic aquí para mostrar/ocultar contraseña"><i class="icon-view mr-0"></i></span>
-											</div>
-										</div>
-										<div class="help-block"></div>
-									</div>
+									<div class="help-block"></div>
 								</div>
 							</div>
-
-							<div class="col-6 col-lg-4 col-xl-6">
-								<div class="field-meter" id="password-strength-meter">
-									<h4>Requerimientos de contraseña:</h4>
-									<ul class="pwd-rules">
-										<li id="length" class="pwd-rules-item rule-invalid">De 8 a 15 <strong>caracteres.</strong></li>
-										<li id="letter" class="pwd-rules-item rule-invalid">Al menos una <strong>letra minúscula.</strong>
-										</li>
-										<li id="capital" class="pwd-rules-item rule-invalid">Al menos una <strong>letra mayúscula.</strong>
-										</li>
-										<li id="number" class="pwd-rules-item rule-invalid">De 1 a 3 <strong>números.</strong></li>
-										<li id="especial" class="pwd-rules-item rule-invalid">Al menos un <strong>carácter especial </strong>(ej: ! @ ? + - . , #).</li>
-										<li id="consecutivo" class="pwd-rules-item rule-invalid">No debe tener más de 2 <strong>caracteres</strong> iguales consecutivos.</li>
-									</ul>
+							<div class="row">
+								<div class="form-group col-12 col-lg-6">
+									<label for="newPassword"><?= lang('NEW_PASSWORD'); ?></label>
+									<div class="input-group">
+										<input id="newPassword" class="form-control" type="password" name="newPassword">
+										<div class="input-group-append">
+											<span id="pwdAddon2" class="input-group-text" title="Clic aquí para mostrar/ocultar contraseña"><i class="icon-view mr-0"></i></span>
+										</div>
+									</div>
+									<div class="help-block"></div>
+								</div>
+								<div class="form-group col-12 col-lg-6">
+									<label for="confirmPassword"><?= lang('CONFIRM_PASSWORD'); ?></label>
+									<div class="input-group">
+										<input id="confirmPassword" class="form-control" type="password" name="confirmPassword">
+										<div class="input-group-append">
+											<span id="pwdAddon3" class="input-group-text" title="Clic aquí para mostrar/ocultar contraseña"><i class="icon-view mr-0"></i></span>
+										</div>
+									</div>
+									<div class="help-block"></div>
 								</div>
 							</div>
 						</div>
 
-						<hr class="separador-one mt-2 mb-4">
-						<div class="flex items-center justify-end">
-							<a class="btn btn-small btn-link" href="<?= base_url('inicio');?>">Cancelar</a>
-							<button id="btnChangePassword" class="btn btn-small btn-loading btn-primary" type="submit" name="btnChangePassword">Continuar</button>
+						<div class="col-6 col-lg-4 col-xl-6">
+							<div class="field-meter" id="password-strength-meter">
+								<h4><?= lang('PASSWORD_INFO_TITLE'); ?></h4>
+								<ul class="pwd-rules">
+									<li id="length" class="pwd-rules-item rule-invalid"><?= lang('PASSWORD_INFO_1'); ?></li>
+									<li id="letter" class="pwd-rules-item rule-invalid"><?= lang('PASSWORD_INFO_2'); ?></li>
+									<li id="capital" class="pwd-rules-item rule-invalid"><?= lang('PASSWORD_INFO_3'); ?></li>
+									<li id="number" class="pwd-rules-item rule-invalid"><?= lang('PASSWORD_INFO_4'); ?></li>
+									<li id="especial" class="pwd-rules-item rule-invalid"><?= lang('PASSWORD_INFO_5'); ?></li>
+									<li id="consecutivo" class="pwd-rules-item rule-invalid"><?= lang('PASSWORD_INFO_6'); ?></li>
+								</ul>
+							</div>
 						</div>
-					</form>
-				</div>
+					</div>
+
+					<hr class="separador-one mt-2 mb-4">
+					<div class="flex items-center justify-end">
+						<a class="btn btn-small btn-link" href="<?= base_url('inicio');?>">Cancelar</a>
+						<button id="btnChangePassword" class="btn btn-small btn-loading btn-primary" type="submit" name="btnChangePassword">Continuar</button>
+					</div>
+				</form>
 			</div>
 		</section>
 	</div>
