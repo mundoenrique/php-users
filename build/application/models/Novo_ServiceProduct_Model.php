88eefc88bf2a911ddd010e69872e2bdfb1ad0262
Pedro Torres <ptorres@novopayment.com>
2020-02-20 09:21:50 -0400

    Asigna nuevo código a código OTP inválido

--- build/application/models/Novo_ServiceProduct_Model.php
+++ build/application/models/Novo_ServiceProduct_Model.php
@@ -317,7 +317,7 @@ class Novo_ServiceProduct_Model extends NOVO_Model
 					];
 					break;
 				case -286:
-					$this->response->code = 3;
+					$this->response->code = 4;
 					$this->response->msg = lang('RESP_CODEOTP_INVALID');
 					$this->response->classIconName = 'ui-icon-info';
 					$this->response->data = [
