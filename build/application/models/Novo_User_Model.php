0a84a7155bb641359661e591955784f547b85126
Pedro Torres <ptorres@novopayment.com>
2020-02-20 12:25:18 -0400

    Condiciona numero de intentos validos OTP

--- build/application/models/Novo_User_Model.php
+++ build/application/models/Novo_User_Model.php
@@ -231,8 +231,9 @@ class Novo_User_Model extends NOVO_Model
 					$this->response->code = 5;
 					$this->response->msg = lang('RESP_CODEOTP_INVALID');
 
-					if ($response->bean->bean == "0") {
-						$this->response->msg = lang('RESP_OTP_FAILED_ATTEMPTS').' ( '.$response->bean.' )';
+					if (json_decode($response->bean)->bean == "0") {
+						$this->response->code = 3;
+						$this->response->msg = lang('RESP_OTP_FAILED_ATTEMPTS').' ( '.$response->bean->bean.' )';
 						$this->response->data = [
 							'btn1' => [
 								'text' => lang('BUTTON_ACCEPT'),
