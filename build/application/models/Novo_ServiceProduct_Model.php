503882e16ba44f6bcebf9bef7e23a04cc8f889a1
Pedro Torres <ptorres@novopayment.com>
2020-02-20 15:47:15 -0400

    Valida en todos los modulos intento fallido OTP

--- build/application/models/Novo_ServiceProduct_Model.php
+++ build/application/models/Novo_ServiceProduct_Model.php
@@ -124,6 +124,19 @@ class Novo_ServiceProduct_Model extends NOVO_Model
 							'action' => 'close'
 						]
 					];
+
+					if ($response->bean == "0") {
+
+						$this->response->code = 3;
+						$this->response->msg = lang('RESP_OTP_FAILED_ATTEMPTS');
+						$this->response->data = [
+							'btn1' => [
+								'text' => lang('BUTTON_ACCEPT'),
+								'link' => FALSE,
+								'action' => 'close'
+							]
+						];
+					}
 					break;
 				case -287:
 					$this->response->code = 3;
@@ -325,6 +338,19 @@ class Novo_ServiceProduct_Model extends NOVO_Model
 							'action' => 'close'
 						]
 					];
+
+					if ($response->bean == "0") {
+
+						$this->response->code = 3;
+						$this->response->msg = lang('RESP_OTP_FAILED_ATTEMPTS');
+						$this->response->data = [
+							'btn1' => [
+								'text' => lang('BUTTON_ACCEPT'),
+								'link' => FALSE,
+								'action' => 'close'
+							]
+						];
+					}
 					break;
 				case -287:
 					$this->response->code = 3;
@@ -739,6 +765,19 @@ class Novo_ServiceProduct_Model extends NOVO_Model
 							'action' => 'close'
 						]
 					];
+
+					if ($response->bean == "0") {
+
+						$this->response->code = 3;
+						$this->response->msg = lang('RESP_OTP_FAILED_ATTEMPTS');
+						$this->response->data = [
+							'btn1' => [
+								'text' => lang('BUTTON_ACCEPT'),
+								'link' => FALSE,
+								'action' => 'close'
+							]
+						];
+					}
 					break;
 				case -287:
 					$this->response->code = 3;
