91325072e9854d6a9aca70a27266ec629ce8b952
epenaloza <epenaloza@novopayment.com>
2020-02-24 13:24:17 -0400

    quita opciones no disponibles en otros paises

--- build/application/helpers/menu_helper.php
+++ build/application/helpers/menu_helper.php
@@ -215,10 +215,10 @@ if (!function_exists('propiedades_menu'))
 					$opcion_menu='{"dashboard":true,"transfer":true,"pago_ve":true,"reportes":true,"service":true,"perfil":true}';
 						break;
 				case 'Co':
-					$opcion_menu='{"dashboard":true,"transfer":true,"pago":true,"reportes":true,"service":true,"perfil":true}';
+					$opcion_menu='{"dashboard":true,"transfer":false,"pago":false,"reportes":true,"service":true,"perfil":true}';
 						break;
 				case 'Pe':
-					$opcion_menu='{"dashboard":true,"transfer_pe":true,"pago":true,"reportes":true,"service":true,"perfil":true}';
+					$opcion_menu='{"dashboard":true,"transfer_pe":false,"pago":false,"reportes":true,"service":true,"perfil":true}';
 						break;
 				case 'Usd':
 					$opcion_menu='{"dashboard":true,"transfer_usd":false,"pago":false,"reportes":true,"service":true,"perfil":true}';
