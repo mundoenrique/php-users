21aab2b51230aefebd0fc9486565559a732a6272
ptorres <ptorres@novopayment.com>
2020-03-30 16:47:23 -0400

    Desabilita tarjeta en lista de producto para atencion al cliente si esta bloqueado

--- build/application/views/serviceproduct/listproduct_content.php
+++ build/application/views/serviceproduct/listproduct_content.php
@@ -15,29 +15,30 @@
 						<input type='hidden' name='noTarjetaConMascara' id='noTarjetaConMascara' value=''>
 						<input type='hidden' name='prefix' id='prefix' value=''>
 					</form>
-					<?php if (count($data) > 0 and $data !== '--'): ?>
-					<?php foreach($data as $row): ?>
-					<?php	$state = (in_array("120", $row['availableServices'])) ? ' inactive' : ''; ?>
-					<div class="dashboard-item big-modal p-1 mx-1 mb-1<?= $state; ?>" id="<?= $row['noTarjeta'];?>" >
-						<img class="item-img" src="<?= $this->asset->insertFile('img-card_gray.svg','img',$countryUri); ?>" alt="Tarjeta gris">
-						<div class="item-info <?= $row['marca'];?> p-2 h5 tertiary bg-white">
-							<p class="item-category semibold primary"><?= $row['nombre_producto'];?></p>
-							<p class="item-cardnumber mb-0"><?= $row['noTarjetaConMascara'];?></p>
-							<?php if ($state === ' inactive'): ?>
-							<button id="generate" class="btn btn-small btn-link" name="generate">Generar PIN </button>
-							<?php elseif ($row['bloqueo'] !== ''): ?>
-							<span class="semibold danger"><?= lang('GEN_TEXT_BLOCK_PRODUCT');?></span>
-							<?php else: ?>
-							<p class="mb-0 h6 light text"><?= strtoupper($row['nomEmp']);?></p>
-							<?php endif; ?>
-						</div>
-					</div>
-					<?php endforeach; ?>
-					<div class="dashboard-item mx-1"></div>
-					<div class="dashboard-item mx-1"></div>
-					<div class="dashboard-item mx-1"></div>
-					<?php else: ?>
-					<h3  class="h4 regular tertiary pt-3"><?= lang('RESP_EMPTY_LIST_PRODUCTS');?></h3>
+					<?php if (count($data) > 0 and $data !== '--') : ?>
+						<?php foreach ($data as $row) : ?>
+							<?php $state = (in_array("120", $row['availableServices']) || count($row['availableServices']) === 0) ? ' inactive' : ''; ?>
+							<?php $title = count($row['availableServices']) === 0 ? lang('GEN_NOT_SERVICES_AVAILABLE') : ''; ?>
+							<div class="dashboard-item big-modal p-1 mx-1 mb-1 <?= $state; ?>" id="<?= $row['noTarjeta']; ?>" title="<?= $title; ?>">
+								<img class=" item-img" src="<?= $this->asset->insertFile('img-card_gray.svg', 'img', $countryUri); ?>" alt="Tarjeta gris">
+								<div class="item-info <?= $row['marca']; ?> p-2 h5 tertiary bg-white">
+									<p class="item-category semibold primary"><?= $row['nombre_producto']; ?></p>
+									<p class="item-cardnumber mb-0"><?= $row['noTarjetaConMascara']; ?></p>
+									<?php if (in_array("120", $row['availableServices'])) : ?>
+										<button id="generate" class="btn btn-small btn-link" name="generate">Generar PIN </button>
+									<?php elseif ($row['bloqueo'] !== '') : ?>
+										<span class="semibold danger"><?= lang('GEN_TEXT_BLOCK_PRODUCT'); ?></span>
+									<?php else : ?>
+										<p class="mb-0 h6 light text"><?= strtoupper($row['nomEmp']); ?></p>
+									<?php endif; ?>
+								</div>
+							</div>
+						<?php endforeach; ?>
+						<div class="dashboard-item mx-1"></div>
+						<div class="dashboard-item mx-1"></div>
+						<div class="dashboard-item mx-1"></div>
+					<?php else : ?>
+						<h3 class="h4 regular tertiary pt-3"><?= lang('RESP_EMPTY_LIST_PRODUCTS'); ?></h3>
 					<?php endif; ?>
 				</div>
 			</div>
--- build/assets/js/helper.js
+++ build/assets/js/helper.js
@@ -25,11 +25,15 @@ var decimalOptions = {
 
 $('input[type=text], input[type=password], input[type=email], input[type=radio]').attr('autocomplete', 'off');
 
-$('.big-modal').on('click', function(){
-	$('.cover-spin').show(0)
+$('.big-modal').on('click', function (e) {
+	if (window.location.pathname.split("/").pop() === 'listaproducto' && this.classList.contains('inactive')) {
+		$(this).off('click');
+	} else {
+		$('.cover-spin').show(0);
+	}
 });
 
-(function() {
+(function () {
 	var actualPage = window.location.pathname.split("/").pop();
 
 	var itemsMenu = $$.getElementsByClassName('nav-item');
@@ -43,7 +47,7 @@ $('.big-modal').on('click', function(){
 		detallereporte: 'reports'
 	};
 
-	if ( actualPage !== 'inicio' && structureMenu.hasOwnProperty(actualPage)){
+	if (actualPage !== 'inicio' && structureMenu.hasOwnProperty(actualPage)) {
 
 		for (var i = 0; i < itemsMenu.length; i++) {
 			itemsMenu[i].classList.remove('active');
@@ -69,15 +73,23 @@ function callNovoCore(verb, who, where, data, _response_) {
 		data: data
 	});
 
-	dataRequest = CryptoJS.AES.encrypt(dataRequest, cpo_cook, { format: CryptoJSAesJson }).toString();
+	dataRequest = CryptoJS.AES.encrypt(dataRequest, cpo_cook, {
+		format: CryptoJSAesJson
+	}).toString();
 	$.ajax({
 		method: verb,
 		url: urlBase + 'async-call',
-		data: { request: dataRequest, cpo_name: cpo_cook, plot: btoa(cpo_cook) },
+		data: {
+			request: dataRequest,
+			cpo_name: cpo_cook,
+			plot: btoa(cpo_cook)
+		},
 		context: document.body,
 		dataType: 'json'
 	}).done(function (response) {
-		response = JSON.parse(CryptoJS.AES.decrypt(response.code, response.plot, { format: CryptoJSAesJson }).toString(CryptoJS.enc.Utf8));
+		response = JSON.parse(CryptoJS.AES.decrypt(response.code, response.plot, {
+			format: CryptoJSAesJson
+		}).toString(CryptoJS.enc.Utf8));
 
 		if (response.code == codeResp) {
 			notiSystem(response.title, response.msg, response.classIconName, response.data);
@@ -126,7 +138,11 @@ function notiSystem(title, message, icon, data) {
 	var title = title || titleNotiSystem;
 	var icon = icon || 'ui-icon-closethick';
 	var message = message || $('#system-msg').text();
-	var btn1 = data.btn1 || { link: false, action: 'close', text: txtBtnAcceptNotiSystem };
+	var btn1 = data.btn1 || {
+		link: false,
+		action: 'close',
+		text: txtBtnAcceptNotiSystem
+	};
 	var btn2 = data.btn2;
 
 	dialogMoldal.dialog({
@@ -136,11 +152,15 @@ function notiSystem(title, message, icon, data) {
 		resizable: false,
 		closeOnEscape: false,
 		minWidth: 370,
-		position: { my: "center top+200", at: "center top", of:  window },
+		position: {
+			my: "center top+200",
+			at: "center top",
+			of: window
+		},
 		dialogClass: "border-none",
-    classes: {
-      "ui-dialog-titlebar": "border-none",
-    },
+		classes: {
+			"ui-dialog-titlebar": "border-none",
+		},
 		show: {
 			duration: 250
 		},
@@ -154,29 +174,29 @@ function notiSystem(title, message, icon, data) {
 			$('#accept, #cancel').removeClass("ui-button ui-corner-all ui-widget");
 
 			$('#accept')
-			.text(btn1.text)
-			.on('click', function(e) {
-				if (btn1.action !== 'wait') {
-					dialogMoldal.dialog('close');
-					$(this).off('click');
-				}
-				if (btn1.action === 'redirect') {
-					$(location).attr('href', btn1.link);
-				}
-			});
+				.text(btn1.text)
+				.on('click', function (e) {
+					if (btn1.action !== 'wait') {
+						dialogMoldal.dialog('close');
+						$(this).off('click');
+					}
+					if (btn1.action === 'redirect') {
+						$(location).attr('href', btn1.link);
+					}
+				});
 
 			$('#cancel').hide();
 			if (btn2) {
 				$('#cancel')
-				.text(btn2.text)
-				.on('click', function(e) {
-					dialogMoldal.dialog('close');
-					if (btn2.action === 'redirect') {
-						$(location).attr('href', btn2.link);
-					}
-					$(this).off('click');
-				})
-				.show();
+					.text(btn2.text)
+					.on('click', function (e) {
+						dialogMoldal.dialog('close');
+						if (btn2.action === 'redirect') {
+							$(location).attr('href', btn2.link);
+						}
+						$(this).off('click');
+					})
+					.show();
 			}
 			dialogMoldal.removeClass("none");
 		}
@@ -205,7 +225,7 @@ function createFields(fields) {
 			label = $(`<label for="${field['id']}">${field.label}</label>`);
 			formGroup = $(`<div class="form-group px-3 pb-1"></div>`).append(label);
 		}
-		formGroup.append(element,`<div class="help-block"></div>`);
+		formGroup.append(element, `<div class="help-block"></div>`);
 		dialogForm.append(formGroup);
 	}
 	return dialogForm;
@@ -214,7 +234,7 @@ function createFields(fields) {
 // Toggle if navbar menu is open or closed
 if (toggleMenu) {
 	toggleMenu.addEventListener('click', function () {
-			collapseMenu.classList.toggle('show');
+		collapseMenu.classList.toggle('show');
 	});
 }
 
@@ -227,10 +247,10 @@ $.datepicker.regional['es'] = {
 	nextText: 'Sig>',
 	currentText: 'Hoy',
 	monthNames: ['Enero', 'Febrero', 'Marzo', 'Abril', 'Mayo', 'Junio', 'Julio', 'Agosto', 'Septiembre', 'Octubre', 'Noviembre', 'Diciembre'],
-	monthNamesShort: ['Ene','Feb','Mar','Abr', 'May','Jun','Jul','Ago','Sep', 'Oct','Nov','Dic'],
+	monthNamesShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dic'],
 	dayNames: ['Domingo', 'Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado'],
-	dayNamesShort: ['Dom','Lun','Mar','Mié','Juv','Vie','Sáb'],
-	dayNamesMin: ['Do','Lu','Ma','Mi','Ju','Vi','Sá'],
+	dayNamesShort: ['Dom', 'Lun', 'Mar', 'Mié', 'Juv', 'Vie', 'Sáb'],
+	dayNamesMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá'],
 	weekHeader: 'Sm',
 	dateFormat: 'dd/mm/yy',
 	firstDay: 1,
@@ -251,8 +271,8 @@ var createElement = function (tagName, attrs) {
 
 	var el = document.createElement(tagName);
 	Object.keys(attrs).forEach((key) => {
-		if (attrs [key] !== undefined) {
-			el.setAttribute(key, attrs [key]);
+		if (attrs[key] !== undefined) {
+			el.setAttribute(key, attrs[key]);
 		}
 	});
 
@@ -261,5 +281,5 @@ var createElement = function (tagName, attrs) {
 
 function formatCurrency(locales, options, number) {
 	var formatted = new Intl.NumberFormat(locales, options).format(number);
-  return formatted;
+	return formatted;
 }
