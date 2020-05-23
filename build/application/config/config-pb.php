379bafa8806cab3eb54e66aceff509c7673fff22
epenaloza <epenaloza@novopayment.com>
2020-05-23 15:57:36 -0400

    Incorpora estilos para pordubanco

--- build/application/config/config-pb.php
+++ build/application/config/config-pb.php
@@ -3,7 +3,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 $config['country'] = 'Ec-pb';
 $config['country-uri'] = 'pb';
-$config['base_url']	= BASE_URL.$config['country-uri'].'/';
+$config['base_url']	= BASE_URL.'/'.$config['country-uri'].'/';
 $config['client'] = 'produbanco';
 $config['favicon'] = 'favicon-bnt';
 $config['language']	= 'spanish-pb';
