b0b125e39d50bd6c4850ec192f6097ceb7f3995c
Hector Corredor <hcorredor@novopayment.com>
2020-06-03 17:15:50 -0500

    implementacion de sección bloque de tarjeta

--- build/application/views/support/services_content-core.php
+++ build/application/views/support/services_content-core.php
@@ -10,7 +10,7 @@
 							<div class="product-presentation relative">
 								<div class="item-network maestro"></div>
 								<div class="product-search">
-									<a class="dialog button product-button"><span aria-hidden="true" class="icon-find"></span></a>
+									<a class="dialog button product-button btn"><span aria-hidden="true" class="icon-find h1 icon-color"></span></a>
 									<input id="donor-cardnumber" name="donor-cardnumber" type="hidden" value="">
 								</div>
 								<img class="card-image" src="../../../assets/images/default/bnt_default.svg" alt="Tarjeta Banorte">
@@ -27,62 +27,62 @@
 					<div class="flex col-12 optional mt-4 px-0">
 						<nav class="nav-config">
 							<ul class="flex flex-wrap justify-between nav-config-box">
-								<li id="" class="list-inline-item nav-item-config mr-0 px-1">
+								<li id="PinManagement" class="list-inline-item nav-item-config mr-0 px-1">
 									<a href="javascript:">
-									<span class="icon-config icon-key h1"></span>
+									<span class="icon-config icon-key h1 icon-color"></span>
 									<h5 class="center">Gestión<br>de PIN</h5>
 									<div class="box up left regular">
-										<span class="icon-key h1"></span>
+										<span class="icon-key h1 icon-color"></span>
 										<h4 class="h5 center">Gestión<br>de PIN</h4>
 									</div>
 									</a>
 								</li>
-								<li id="" class="list-inline-item nav-item-config mr-0 px-1">
+								<li id="CardLock" class="list-inline-item nav-item-config mr-0 px-1">
 									<a href="javascript:">
-									<span class="icon-config icon-lock h1"></span>
+									<span class="icon-config icon-lock h1 icon-color"></span>
 									<h5 class="center">Bloqueo<br>de tarjeta</h5>
 									<div class="box up left regular">
-										<span class="icon-lock h1"></span>
+										<span class="icon-lock h1 icon-color"></span>
 										<h4 class="h5 center">Bloqueo<br>de tarjeta</h4>
 									</div>
 									</a>
 								</li>
-								<li id="" class="list-inline-item nav-item-config mr-0 px-1">
+								<li id="replacementRequest" class="list-inline-item nav-item-config mr-0 px-1">
 									<a href="javascript:">
-									<span class="icon-config icon-spinner h1"></span>
+									<span class="icon-config icon-spinner h1 icon-color"></span>
 									<h5 class="center">Solicitud<br>de reposición</h5>
 									<div class="box up left regular">
-										<span class="icon-spinner h1"></span>
+										<span class="icon-spinner h1 icon-color"></span>
 										<h4 class="h5 center">Solicitud<br>de reposición</h4>
 									</div>
 									</a>
 								</li>
 								<li id="" class="list-inline-item nav-item-config mr-0 px-1">
 									<a href="javascript:">
-									<span class="icon-config icon-spinner h1"></span>
+									<span class="icon-config icon-spinner h1 icon-color"></span>
 									<h5 class="center">Solicitud<br>de reposición</h5>
 									<div class="box up left regular">
-										<span class="icon-spinner h1"></span>
+										<span class="icon-spinner h1 icon-color"></span>
 										<h4 class="h5 center">Solicitud<br>de reposición</h4>
 									</div>
 									</a>
 								</li>
 								<li id="" class="list-inline-item nav-item-config mr-0 px-1">
 									<a href="javascript:">
-									<span class="icon-config icon-spinner h1"></span>
+									<span class="icon-config icon-spinner h1 icon-color"></span>
 									<h5 class="center">Solicitud<br>de reposición</h5>
 									<div class="box up left regular">
-										<span class="icon-spinner h1"></span>
+										<span class="icon-spinner h1 icon-color"></span>
 										<h4 class="h5 center">Solicitud<br>de reposición</h4>
 									</div>
 									</a>
 								</li>
 								<li id="" class="list-inline-item nav-item-config mr-0 px-1">
 									<a href="javascript:">
-									<span class="icon-config icon-spinner h1"></span>
+									<span class="icon-config icon-spinner h1 icon-color"></span>
 									<h5 class="center">Solicitud<br>de reposición</h5>
 									<div class="box up left regular">
-										<span class="icon-spinner h1"></span>
+										<span class="icon-spinner h1 icon-color"></span>
 										<h4 class="h5 center">Solicitud<br>de reposición</h4>
 									</div>
 									</a>
@@ -95,7 +95,7 @@
 		</div>
 
 		<div class="col-12 col-sm-12 col-lg-6 col-xl-8">
-			<div id="notificationsView">
+			<div id="pinManagementView">
 				<div class="flex mb-1 mx-4 flex-column">
 					<h4 class="line-text mb-2 semibold primary">Gestión de PIN</h4>
 					<div id="pre-loader" class="mt-5 mx-auto flex justify-center">
@@ -154,6 +154,56 @@
 							</form>
 						</div>
 					</div>
+				</div>
+			</div>
+
+			<div id="cardLockView">
+				<div class="flex mb-1 mx-4 flex-column">
+					<h4 class="line-text mb-2 semibold primary">Bloqueo de tarjeta</h4>
+					<p>Si realmente deseas bloqueas tu tarjeta, presiona continuar</p>
+						<div class="flex items-center justify-end pt-3">
+							<a class="btn btn-small btn-link" href="">Cancelar</a>
+							<button id="btnChange" class="btn btn-small btn-loading btn-primary" type="submit" name="btnChange">Continuar</button>
+						</div>
+					</div>
+				</div>
+			</div>
+
+		<div id="replacementRequestView">
+			<div class="flex mb-1 mx-4 flex-column">
+				<h4 class="line-text mb-2 semibold primary">Bloqueo de tarjeta</h4>
+				<p>Seleccione una motivo de la solicitud</p>
+				<form id="formReplace" class="profile-1" method="post">
+					<div class="row">
+						<div class="form-group col-lg-4">
+							<label for="replaceMotSol">Motivo de la solicitud</label>
+							<select id="replaceMotSol" class="custom-select form-control" name="replaceMotSol">
+								<option value="">Selecciona</option>
+								<option value="59">Tarjeta robada</option>
+								<option value="17">Sospecha de fraude</option>
+								<option value="41">Tarjeta cancelada</option>
+							</select>
+							<div class="help-block"></div>
+						</div>
+					</div>
+					<div id="replaceVerificationOTP">
+						<hr class="separador-one mb-3">
+						<p>Hemos enviado un código de verificación a tu teléfono móvil, por favor indícalo a continuación:</p>
+						<div class="row">
+							<div class="form-group col-lg-4">
+								<label for="replaceCodeOTP">Código de verificación</label>
+								<input id="replaceCodeOTP" class="form-control" type="text" name="replaceCodeOTP" disabled="" autocomplete="off">
+								<div id="replaceTxtMsgErrorCodeOTP" class="help-block"></div>
+							</div>
+						</div>
+						<p id="replaceVerificationMsg" class="mb-1 h5"></p>
+					</div>
+					<hr class="separador-one">
+					<div class="flex items-center justify-end pt-3">
+						<a class="btn btn-small btn-link" href="">Cancelar</a>
+						<button id="btnReplace" class="btn btn-small btn-loading btn-primary" type="submit" name="btnReplace">Continuar</button>
+					</div>
+				</form>
 				</div>
 			</div>
 		</div>
