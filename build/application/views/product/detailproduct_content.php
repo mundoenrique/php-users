3e2b1f27c354fa05e768b30498d408f2ab67b71a
jhllerena <jhllerena@novopayment.com>
2020-06-25 12:57:15 -0400

    Resalta opción para descargar extracto en detalles

--- build/application/views/product/detailproduct_content.php
+++ build/application/views/product/detailproduct_content.php
@@ -116,15 +116,15 @@
 							<input id="optionTransit" type="radio" name="movimientos" disabled> En tránsito
 						</label>
 					</div>
-					<ul class="stack list-inline mb-0 flex items-center">
+					<ul class="stack list-inline mb-0 flex items-stretch">
 						<li class="stack-item px-1 list-inline-item rounded">
 							<a id="downloadPDF" href="#" rel="subsection"><span class="icon-file-pdf h5 mr-0" aria-hidden="true" title="Descargar PDF"></span></a>
 						</li>
 						<li class="stack-item px-1 list-inline-item rounded">
 							<a id="downloadXLS" href="#" rel="subsection"><span class="icon-file-excel h5 mr-0" aria-hidden="true" title="Descargar Excel"></span></a>
 						</li>
-						<li class="stack-item px-1 list-inline-item rounded is-disabled">
-							<a id="downloadExtract" href="#" rel="subsection"><span class="icon-download h5 mr-0" aria-hidden="true" title="Descargar Extracto"></span></a>
+						<li class="stack-item stack-item-primary px-1 list-inline-item rounded">
+							<a id="downloadExtract" href="#" title="Descargar Extracto" rel="subsection">Descargue su extracto</a>
 						</li>
 					</ul>
 				</nav>
