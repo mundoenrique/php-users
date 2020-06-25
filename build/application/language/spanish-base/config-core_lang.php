02dfcfa0beb0ba16853c13a7d9c11b99499ae241
epenaloza <epenaloza@novopayment.com>
2020-06-24 22:53:00 -0400

    Mejora evaluación de soporte a navegadores

--- build/application/language/spanish-base/config-core_lang.php
+++ build/application/language/spanish-base/config-core_lang.php
@@ -1,5 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
+//SUPPORT IE 11
+$lang['CONF_SUPPORT_IE'] = 'OFF';
 //VALIDATE FORMS
 $lang['CONF_VALID_ERROR'] = 'has-error';
 $lang['CONF_VALID_VALID'] = 'has-success';
