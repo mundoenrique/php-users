ea8196c0d71225cda5c728ae8b96dfbb62d0074a
Hector Corredor <hcorredor@novopayment.com>
2020-07-24 16:27:49 -0500

    Ajusta archivos en de lenguajes

--- build/application/language/spanish-base/customersupport_lang.php
+++ build/application/language/spanish-base/customersupport_lang.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+//GENERAL LANGUAGE
 $lang['CUST_TWIRLS_COMMERCIAL'] = 'Giros comerciales';
 $lang['CUST_TRANS_LIMITS'] = 'Limites transaccionales';
 $lang['CUST_CARD_NUMBER'] = 'Nro. de tarjeta';
--- build/application/language/spanish-bnt/customersupport_lang.php
+++ build/application/language/spanish-bnt/customersupport_lang.php
@@ -1,4 +1,4 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
+//GENERAL LANGUAGE
 $lang['CUST_DNI'] = 'CURP';
