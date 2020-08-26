380957836e6d555c4d60e0bdee05705b2dc40b42
jhllerena <jhllerena@novopayment.com>
2020-08-26 12:05:53 -0400

    Incorpora campos para documento de empresa

--- build/assets/js/bdb/user/recoveryaccess.js
+++ build/assets/js/bdb/user/recoveryaccess.js
@@ -14,7 +14,8 @@ $$.addEventListener('DOMContentLoaded', function(){
 		if(form.valid()) {
 
 			var txtbtnTrigger = btnTrigger.innerHTML.trim();
-			var typeDocument = $$.getElementById('typeDocument');
+			var typeDocumentUser = $$.getElementById('typeDocumentUser');
+			var typeDocumentBussines = $$.getElementById('typeDocumentBussines');
 			var data = {};
 
 			btnTrigger.innerHTML = msgLoadingWhite;
@@ -32,11 +33,19 @@ $$.addEventListener('DOMContentLoaded', function(){
 				}
 			);
 
-			var codeTypeDocument = typeDocument.options[typeDocument.selectedIndex].value;
-			var abbrTypeDocument = dataForm.typeDocument.find(function(e){return e['id'] == codeTypeDocument}).abreviatura
+			var codeTypeDocumentUser = typeDocumentUser.options[typeDocumentUser.selectedIndex].value;
+			var codeTypeDocumentBussines = typeDocumentBussines.options[typeDocumentBussines.selectedIndex].value;
+			var abbrTypeDocumentUser = dataForm.typeDocument.find(function (e) {
+				return e['id'] == codeTypeDocumentUser
+			}).abreviatura;
+			var abbrTypeDocumentBussines = dataForm.typeDocument.find(function (e) {
+				return e['id'] == codeTypeDocumentBussines
+			}).abreviatura;
 
-			data['codeTypeDocument'] = codeTypeDocument;
-			data['abbrTypeDocument'] = abbrTypeDocument;
+			data['codeTypeDocumentUser'] = codeTypeDocumentUser;
+			data['abbrTypeDocumentUser'] = abbrTypeDocumentUser;
+			data['codeTypeDocumentBussines'] = codeTypeDocumentBussines;
+			data['abbrTypeDocumentBussines'] = abbrTypeDocumentBussines;
 
 			callNovoCore('POST', 'User', 'recoveryAccess', data, function(response) {
 				btnTrigger.innerHTML = txtbtnTrigger;
