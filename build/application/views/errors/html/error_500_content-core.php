77fadbede8120cd260e73a144cc619a27dd22c7c
jcadiz <jcadiz@novopayment.com>
2020-09-10 15:03:35 -0400

    Incluye maquetación de vista error 404 y 500

---
+++ build/application/views/errors/html/error_500_content-core.php
@@ -0,0 +1,10 @@
+<?php defined('BASEPATH') OR exit('No direct script access allowed'); ?>
+<div class="pt-3 px-5 pb-5 bg-color">
+	<div class="logout-content flex flex-column items-center justify-center">
+		<i class="icon icon-500 bug-view" aria-hidden="true"></i>
+		<h1 class="tertiary semibold h01">500</h1>
+		<span class="tertiary regular h3 uppercase mb-2">¡Vaya algo salió mal!</span>
+		<span class="tertiary regular h3">Trata de volver a cargar esta página o no dudes en contactar con nosotros si el problema persiste.</span>
+		<a class="btn btn-primary my-5" href="javascript:history.back()">Volver</a>
+	</div>
+</div>
