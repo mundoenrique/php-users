a3ec9b70077c847015c12415c42929e64ccabba9
epenaloza <epenaloza@novopayment.com>
2020-09-24 07:45:32 -0400

    incluye config soles y dólares

--- build/application/config/config-us.php
+++ build/application/config/config-us.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$config['country'] = 'Pe';
-$config['country-uri'] = 'pe';
+$config['country'] = 'Usd';
+$config['country-uri'] = 'us';
 $config['base_url']	= BASE_URL.'/'.$config['country-uri'].'/';
 $config['language']	= 'spanish-pe';
