3ad287cb04b7aa9fce95109d1f29b8cf73bacee0
jhllerena <jhllerena@novopayment.com>
2020-11-23 15:50:05 -0400

    Modifica mensajes atención al cliente - produbanco

--- build/application/language/spanish-pb/customerSupport_lang.php
+++ build/application/language/spanish-pb/customerSupport_lang.php
@@ -1,10 +1,15 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['CUST_SUCCESS_CHANGE_PIN'] = 'Tu transacción ha sido procesada exitosamente.';
-$lang['CUST_PIN_NOT_VALID'] = 'Los datos ingresados son incorrectos.';
+$lang['CUST_SUCCESS_OPERATION_RESPONSE'] = 'Tu transacción ha sido procesada exitosamente.';
+$lang['CUST_PIN_NOT_VALID'] = 'El PIN actual es incorrecto, verifica e intenta nuevamente.';
 $lang['CUST_DATA_INVALIDATED'] = 'Los datos ingresados son incorrectos.';
 $lang['CUST_FAILED_ATTEMPTS'] = 'En este momento no podemos procesar tu solicitud, por favor intenta más tarde.';
 $lang['CUST_PIN_NOT_CHANGED'] = 'En este momento no podemos procesar tu solicitud, por favor intenta más tarde.';
 $lang['CUST_INVALID_EXPIRATION_DATE'] = 'En este momento no podemos procesar tu solicitud, por favor intenta más tarde.';
 $lang['CUST_EXPIRED_CARD'] = 'En este momento no podemos procesar tu solicitud, por favor intenta más tarde.';
 $lang['CUST_NOT_PROCCESS'] = 'En este momento no podemos procesar tu solicitud, por favor intenta más tarde.';
+$lang['CUST_LOCK_CARD'] = 'En este momento no podemos procesar tu solicitud, por favor intenta más tarde.';
+$lang['CUST_SPECIFIC_PERMANENT_LOCK'] = 'En este momento no podemos procesar tu solicitud, por favor intenta más tarde.';
+$lang['CUST_SPECIFIC_REVEWAL_LOCK'] = 'En este momento no podemos procesar tu solicitud, por favor intenta más tarde.';
+$lang['CUST_LIMIT_REPLACEMENTS'] = 'En este momento no podemos procesar tu solicitud, por favor intenta más tarde.';
