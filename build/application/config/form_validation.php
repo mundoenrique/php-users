644aa969ccb0e202459edca98c2499c03e4835a5
epenaloza <epenaloza@novopayment.com>
2021-01-07 22:25:16 -0400

    Valida clave de usuario

--- build/application/config/form_validation.php
+++ build/application/config/form_validation.php
@@ -9,7 +9,7 @@ $config = [
 		],
 		[
 			'field' => 'userPass',
-			'rules' => 'trim|regex_match[/^([a-zA-Z0-9=]+)+$/i]|required'
+			'rules' => 'trim|regex_match[/^([\w!@\*\-\?¡¿+\/.,_#]+)+$/i]|required'
 		],
 		[
 			'field' => 'currentTime',
