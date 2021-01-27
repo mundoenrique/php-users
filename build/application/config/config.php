1c8b9c89c5f63f510f691de09e931a1f5ab0307d
epenaloza <epenaloza@novopayment.com>
2021-01-26 23:52:35 -0400

    configura archivos config

--- build/application/config/config.php
+++ build/application/config/config.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') or exit('No direct script access allowed');
+defined('BASEPATH') OR exit('No direct script access allowed');
 
 /*
 |--------------------------------------------------------------------------
@@ -26,33 +26,6 @@ defined('BASEPATH') or exit('No direct script access allowed');
 $config['base_url'] = BASE_URL;
 $config['asset_url'] = BASE_CDN_URL;
 $config['asset_path'] = BASE_CDN_PATH;
-$config['urlWS'] = WS_URL;
-$config['keyNovo'] = WS_KEY;
-$config['active_recaptcha'] = ACTIVE_RECAPTCHA;
-$config['client'] = 'novo';
-$config['channel'] = 'personasWeb';
-$config['keyAES256'] = KEY_AES256;
-$config['ivAES256'] = IV_AES256;
-
-//url API
-$config['urlAPI'] = URL_API;
-//Credenciales oauth
-$config['clientId'] = CLIENT_ID;
-$config['clientSecret'] = CLIENT_SECRET;
-$config['format_date'] = 'j/m/Y';
-$config['format_time'] = 'g:i A';
-$config['oauth_url'] = OAUTH_URL;
-$config['scores_recapcha'] = [
-	'development' => [
-		'score' => 0
-	],
-	'testing' => [
-		'score' => 0.3
-	],
-	'production' => [
-		'score' => 0.4
-	],
-];
 
 /*
 |--------------------------------------------------------------------------
@@ -413,10 +386,6 @@ $config['sess_save_path'] = SESS_SAVE_PATH;
 $config['sess_match_ip'] = SESS_MATCH_IP;
 $config['sess_time_to_update'] = ACTIVE_SAFETY == 'ON' ? 30 : 0;
 $config['sess_regenerate_destroy'] = TRUE;
-/*
-| Configura control de tiempo de sesión
-*/
-$config['session_time'] = SESS_EXPIRATION * 1000;
 
 /*
 |--------------------------------------------------------------------------
