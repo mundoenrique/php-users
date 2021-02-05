df1ef4ed0ecc1dce79b09d53a2f63d882b9f0481
epenaloza <epenaloza@novopayment.com>
2021-02-04 23:28:33 -0400

    Incluye formato de moneda para Venezuela

--- build/application/language/spanish-ve/general_lang.php
+++ build/application/language/spanish-ve/general_lang.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 //GENERAL LANGUAGE
+$lang['GEN_CURRENCY'] = 'Bs.';
 $lang['GEN_CONTACTS'] = [
 	'0500-BONUS-00' => '(0500-26687-00)',
 	'0501-PLATA-00' => '(0501-75282-00)',
