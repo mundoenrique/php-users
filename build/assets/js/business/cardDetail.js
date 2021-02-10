a416787bd8213bed44bed520e83fb44fed1aaaaa
Hector Corredor <hcorredor@novopayment.com>
2021-02-10 17:15:02 -0500

    Incorpora funcion del datapicker

--- build/assets/js/business/cardDetail.js
+++ build/assets/js/business/cardDetail.js
@@ -19,6 +19,34 @@ $(function () {
 		}
 	});
 
+	$("#monthYear").datepicker({
+		dateFormat: 'mm/yy',
+		changeMonth: true,
+		changeYear: true,
+		showButtonPanel: true,
+		maxDate: "+0D",
+		closeText: 'Aceptar',
+		yearRange: '-12:' + currentDate.getFullYear(),
+
+		onSelect: function (selectDate) {
+			$(this)
+				.focus()
+				.blur();
+		},
+
+		onClose: function (dateText, inst) {
+			var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
+			var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+			$(this).datepicker('setDate', new Date(year, month, 1));
+		},
+		beforeShow: function (input, inst) {
+			var month = $("#ui-datepicker-div .ui-datepicker-month :selected").val();
+			var year = $("#ui-datepicker-div .ui-datepicker-year :selected").val();
+			inst.dpDiv.addClass("ui-datepicker-month-year");
+			$(this).datepicker('setDate', new Date(year, month, 1));
+		}
+	});
+
 	$('#search').on('click', function(e) {
 		e.preventDefault();
 
