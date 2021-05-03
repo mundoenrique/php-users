a3fd5ee749012f2167c98177a63e97701b6519e4
epenaloza <epenaloza@novopayment.com>
2021-05-03 19:50:36 -0400

    aumenta tiempo de sesión del lado del servidor

--- build/application/config/config.php
+++ build/application/config/config.php
@@ -381,10 +381,10 @@ $config['encryption_key'] = ENCRYPTION_KEY;
 */
 $config['sess_driver'] = SESS_DRIVER;
 $config['sess_cookie_name'] = SESS_COOKIE_NAME;
-$config['sess_expiration'] = SESS_EXPIRATION > 0 ? SESS_EXPIRATION + 50 : SESS_EXPIRATION;
+$config['sess_expiration'] = SESS_EXPIRATION > 0 ? SESS_EXPIRATION + 70 : SESS_EXPIRATION;
 $config['sess_save_path'] = SESS_SAVE_PATH;
 $config['sess_match_ip'] = TRUE;
-$config['sess_time_to_update'] = ACTIVE_SAFETY == 'ON' ? 30 : 0;
+$config['sess_time_to_update'] = ACTIVE_SAFETY == 'ON' ? 60 : 0;
 $config['sess_regenerate_destroy'] = TRUE;
 
 /*
