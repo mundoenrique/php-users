e2f1a41c7990d92c951df7b59963677ce90d9233
jcadiz <jcadiz@novopayment.com>
2021-05-03 14:24:51 -0400

    Agrega config para voygo

---
+++ build/application/config/config-vg.php
@@ -0,0 +1,8 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$config['country'] = 'Pe';
+$config['country-uri'] = 'vg';
+$config['base_url']	= BASE_URL.'/'.$config['country-uri'].'/';
+$config['client'] = 'voygo';
+$config['language']	= 'spanish-bg';
