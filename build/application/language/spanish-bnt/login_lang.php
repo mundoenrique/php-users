02cce67b3b39c383c0ae61672573febfc2e834c3
epenaloza <epenaloza@novopayment.com>
2021-05-12 12:22:55 -0400

    Elimina archivo no usado

--- build/application/language/spanish-bnt/login_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-//GENERAL LANGUAGE
-$lang['LOGIN_WIDGET_TITLE'] = 'Personas';
-$lang['LOGIN_LOGO_WIDGET'] = 'img-banorte.png';
-$lang['LOGIN_IMAGE'] = 'banorte-login.jpg';
