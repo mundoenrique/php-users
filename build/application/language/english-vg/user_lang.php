98c072d83b8337338460634b19fc973470fc79af
jvega <jvega@novopayment.com>
2021-05-13 10:37:44 -0400

    Agrega contrato de Terminos y Condiciones de Voygo

---
+++ build/application/language/english-vg/user_lang.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$lang["USER_TERMS_CONTENT"] = '
+<h2><b>Condiciones Generales, Términos de Uso y Confidencialidad.</b></h2>
+
+<p>Servitebca Perú, Electronic Transfer of Benefits and Payments Service S.A., a company domiciled in the city of Lima and registered in electronic entry 12072298, on October 19, 2007, who will hereinafter be called “The Company”; The terms and conditions governing the use and confidentiality of the Companys electronic platform are described below; hereinafter "The Platform".</p>
+
+<p>Its Users are obliged to read these General Conditions and Terms of Use and Confidentiality carefully before continuing their tour on this website and, if they do, it will be understood that they agree with them and declare that they agree to fully comply with the instructions. and obligations contained herein.</p>
+
+<p>The Company is not responsible for the security measures that must be taken by the Users, when they obtain their personal passwords and passwords, nor for the inappropriate use of their protection; In this sense, it is released from all responsibility in relation to the misuse of keys, passwords, electronic signatures, certificates and even the hardware itself where privileged information is stored. This release of liability extends to any damage caused by the Users derived from the misuse of the information contained in the Platform. Likewise, The Company is not responsible for non-compliance or delay in the provision of its services, as long as it is the consequence of a fortuitous event and / or force majeure and for as long as these persist.</p>
+
+<p>The Companys Platform provides its Users with the necessary tools to comply with and make use of the different programs it offers efficiently, quickly and with total security. However, The Company does not guarantee in any way the uninterrupted or error-free service through this website.</p>
+
+<p>This Company Platform offers the link to other websites so that access to them will be at the Users risk and expense. Likewise, Users will be able to find advertisements referring to the promotions of the Companys products and services, which will be governed by the contracts that regulate them as well as by the terms and conditions that govern the respective promotions.</p>
+
+<p>The Company declares that the data, information and in general the products and services offered by it, when they belong to or refer to the User, are subject to the constitutional, legal and administrative provisions that guarantee the rights to privacy of communications and access. to personal information, according to the provisions of paragraph 6 of article 2 of the Political Constitution of Peru and in Law 29733, Law of Protection of Personal Data and its Regulations, approved by Supreme Decree 003-2013-JUS.</p>
+
+<p>In this sense, Users understand that the information contained, posted and made available to the public on The Companys Platform are considered data messages, according to the terms and conditions set forth herein. Regarding data messages, the rights to privacy of communications and access to personal information are guaranteed.</p>
+
+<p>The Company declares that the data messages are protected by security devices that provide, to verify that they have not been altered, that they have remained intact during their generation, communication and transmission, reception and filing, and to confirm the identity of the issuer, with this it can show its origin. Also, these data messages are recorded and can be retrieved for later reference.</p>
+
+<p>Likewise, The Company declares that all intellectual creations, trademarks, slogans, domain names, designs and any other intangible good, whether or not they are protected under the Intellectual Property regulations: Industrial Property and Copyright, will have the sole owner of the economic rights over the intangible objects protected to The Company, who is the only one that may use them in accordance with the protection and powers granted to them in Peru, the current intellectual property legal system. </p>
+
+<p>The Company will make its best efforts for the proper use of its technological platforms, guaranteeing the honor and personal and family privacy of its Users, as understood by the latter. However, The Company reserves the right to temporarily suspend the service at any time without prior notification to the Users, as long as it is for maintenance reasons of the Platform or other technical reasons that are considered necessary and convenient. In these cases, there will be no type of compensation from The Company to the Users.</p>
+
+<p>Users, in accordance with the security and privacy policies of The Platform, may revoke their consent or authorization through any of the service channels maintained by The Company, if they do not wish to receive commercial electronic messages, in which case, The Company will cease said shipments and will apply the corresponding friendly corrective measures.</p>
+
+<p>The Company reserves, at its sole discretion, to use all the necessary means at its disposal to immediately withdraw the user who violates any of the security and privacy conditions of The Platform, which includes that Users have sent false information that does not is capable of verifying it as to its authenticity.</p>
+
+<p>Likewise, The Company has the right to change the security and privacy policies, and all changes will be published on The Platform for the purpose of notifying its Users.</p>
+
+<p>Thus, in the event of any violation of the security and privacy conditions, of the systems that use information technology, of the privacy of people and communications and of the intellectual property that are linked to The Platform, the provisions will apply in the Criminal Code on Computer Crimes and other current and applicable regulations of the Peruvian legal system.</p>
+
+<h2><b>General Conditions, Terms of Use and Confidentiality.</b></h2>
+
+<p>It is reported that the personal data provided to Servitebca Perú, Electronic Transfer of Benefits and Payments Service S.A. ("SERVITEBCA") are incorporated into SERVITEBCAs customer base. SERVITEBCA will use said information for the purpose of providing the requested and / or contracted services (including data processing, formalization of contracts, sending correspondence, among others), which may be carried out through third parties.</p>
+
+<p>Likewise, the owner of the personal data authorizes SERVITEBCA to use, as long as this authorization is not revoked, their personal data, including sensitive data, which would have been provided directly to SERVITEBCA, those that could be found in sources accessible to the public or the that have been obtained from third parties; for treatments that involve the development of commercial actions, including the referral (via physical, electronic or telephone means) of advertising, information or offers / promotions (personalized or general) of SERVITEBCA services and / or other companies of the Intercorp Group and its partners strategies, among which are those published on the website of the Superintendency of the Securities Market (www.smv.gob.pe) as well as on the website www.intercorp.com.pe/es.</p>
+
+<p>For such purposes, the owner of the personal data authorizes SERVITEBCA the assignment, transfer or communication of their personal data, to said companies and between them. SERVITEBCA may use, provide and / or transfer the personal data provided to comply with the obligations and / or requirements that are generated by virtue of the regulations in force in the Peruvian legal system, including but not limited to those linked to the prevention system of money laundering and financing of terrorism and prudential regulations. SERVITEBCA may treat and eventually transfer personal data to authorities and third parties authorized by law.</p>
+
+<p>The owner of the personal data is informed that they can revoke the authorization for the processing of their personal data at any time, in accordance with the provisions of the Personal Data Protection Law (Law No. 29733) and its Regulations (Supreme Decree No. 003-2013-JUS To exercise this right, or any other provided in said regulations, the owner of personal data may submit their request at the SERVITEBCA office or through the Contact Center or any other channel that the company reports in the future.</p>
+';
