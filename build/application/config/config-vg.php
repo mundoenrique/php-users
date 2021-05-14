cea22f4f980b9f10faa1d9d8e8d79bf664cb5d0c
epenaloza <epenaloza@novopayment.com>
2021-05-14 13:47:12 -0400

    Ajusta cliente para servicios

--- build/application/config/config-vg.php
+++ build/application/config/config-vg.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$config['customer'] = 'Pe';
+$config['customer'] = 'Usd';
 $config['customer-uri'] = 'vg';
 $config['base_url']	= BASE_URL.'/'.$config['customer-uri'].'/';
 $config['language']	= BASE_LANGUAGE.'-'.$config['customer-uri'];
