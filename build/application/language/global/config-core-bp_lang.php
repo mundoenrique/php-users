7b667547eb7db85290ac9e9ac07137b5624f0103
jcadiz <jcadiz@novopayment.com>
2021-06-22 16:29:23 -0400

    Oculta widget de contacto para Pichincha

--- build/application/language/global/config-core-bp_lang.php
+++ build/application/language/global/config-core-bp_lang.php
@@ -2,5 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 //SIGNIN
 $lang['CONF_SIGNIN_WELCOME_MSG'] = 'ON';
+$lang['CONF_SIGNIN_WIDGET_CONTACT'] = 'OFF';
 //FOOTER
 $lang['CONF_FOOTER_INFO'] = 'OFF';
