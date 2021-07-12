b245c6a6ada686dc83be80e4b131a36c3930c511
epenaloza <epenaloza@novopayment.com>
2021-07-12 19:50:16 -0400

    Configura carpeta de lenguaje

--- build/application/config/config-bog.php
+++ build/application/config/config-bog.php
@@ -3,4 +3,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $config['customer'] = 'Bdb';
 $config['customer-uri'] = 'bog';
 $config['base_url']	= BASE_URL.'/'.$config['customer-uri'].'/';
-$config['language']	= BASE_LANGUAGE.'-'.$config['customer-uri'];
+$config['language']	= BASE_LANGUAGE.'-bdb';//.$config['customer-uri'];
