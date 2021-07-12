38f2a8235974d45d01b73bfd266eebe06142830c
epenaloza <epenaloza@novopayment.com>
2021-07-12 12:51:30 -0400

    Incluye bdb en configuración

--- build/application/language/global/config-core_lang.php
+++ build/application/language/global/config-core_lang.php
@@ -81,6 +81,7 @@ $lang['CONF_COUNTRY_CODE'] = [
 	'Ec-pb' => 3,
 	'Ec-bg' => 3,
 	'Co' => 4,
+	'Bdb' => 4,
 	'Mx-Bn' => 15,
 ];
 $lang['CONF_COUNTRY_DOCUMENT'] = [
@@ -91,6 +92,7 @@ $lang['CONF_COUNTRY_DOCUMENT'] = [
 	'Ec-pb' => '3',
 	'Ec-bg' => '3',
 	'Co' => '4',
+	'Bdb' => '4',
 	'Mx-Bn' => '16',
 ];
 //SIGNUP-PROFILE
