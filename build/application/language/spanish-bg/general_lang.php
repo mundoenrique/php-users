1741917b0be0b778a22ff9499007a2295bb529bd
epenaloza <epenaloza@novopayment.com>
2021-07-12 19:49:48 -0400

    Elimina archivos innecesarios

--- build/application/language/spanish-bg/general_lang.php
+++
@@ -1,9 +0,0 @@
-<?php
-defined('BASEPATH') OR exit('No direct script access allowed');
-//GENERAL LANGUAGE
-$lang['GEN_CONTACTS_CITY'] = 'CIUDAD';
-$lang['GEN_CONTACTS_CONTACT'] = 'CONTACTO';
-$lang['GEN_CONTACTS_TITLE'] = 'Líneas de atención a nivel nacional';
-$lang['GEN_CONTACTS'] = [
-	'XXXXX' => 'XXXXXXX',
-];
