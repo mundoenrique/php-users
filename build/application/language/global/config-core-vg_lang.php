002c518f809d0dcca6eee49f8ca0ea898f70e544
jvega <jvega@novopayment.com>
2021-07-27 10:35:43 -0400

    Desactiva notificaciones para Voygo

--- build/application/language/global/config-core-vg_lang.php
+++ build/application/language/global/config-core-vg_lang.php
@@ -1,7 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['CONF_SEND_MOVEMENTS'] = 'ON';
-$lang['CONF_NOTIFICATIONS'] = 'ON';
 $lang['CONF_HEADER_BORDER'] = 'ON';
 //CARDS DETAIL
 $lang['CONF_TYPE_TRANSACTION'] = 'ON';
