730f6a9a8a909370991a9618f54b6e742375824f
epenaloza <epenaloza@novopayment.com>
2021-09-20 22:37:59 -0400

    Elimina método innecesario

--- build/application/controllers/Novo_Reports.php
+++ build/application/controllers/Novo_Reports.php
@@ -65,24 +65,4 @@ class Novo_Reports extends NOVO_Controller {
 		$this->views = ['reports/'.$view];
 		$this->loadView($view);
 	}
-	/**
-	 * @info Método para obtener las opciones de notificación del cliente
-	 * @author J. Enrique Peñaloza Piñero.
-	 * @date May 21th, 2020
-	 */
-	public function notifications()
-	{
-		log_message('INFO', 'NOVO CustomerSupport: notifications Method Initialized');
-
-		$view = 'notifications';
-		array_push(
-			$this->includeAssets->jsFiles,
-			"support/notifications"
-		);
-
-		$this->render->activeHeader = TRUE;
-		$this->render->titlePage = lang('GEN_MENU_NOTIFICATIONS');
-		$this->views = ['support/'.$view];
-		$this->loadView($view);
-	}
 }
