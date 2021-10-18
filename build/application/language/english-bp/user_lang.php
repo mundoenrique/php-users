d04b4e93ff1fb63a456ca6325416a1c3a8663e09
epenaloza <epenaloza@novopayment.com>
2021-10-18 17:10:29 -0400

    Cambia variables de lenguaje

--- build/application/language/english-bp/user_lang.php
+++ build/application/language/english-bp/user_lang.php
@@ -1,4 +1,3 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['USER_SIGNIN_TITLE'] = 'Welcome to Customers Online';
-$lang['USER_WELCOME_MSG'] = 'Direct access to your account / card for inquiries and operations, 7x24.';
