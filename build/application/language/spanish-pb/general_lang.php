4603603382b8112ab6accd736c3430d649de4776
lmolina <lmolina@novopayment.com>
2021-10-28 11:41:40 -0400

    Actualiza mensajes de datos incorrectos

--- build/application/language/spanish-pb/general_lang.php
+++ build/application/language/spanish-pb/general_lang.php
@@ -4,3 +4,4 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $lang['GEN_SYSTEM_MESSAGE'] = 'En este momento no podemos procesar tu solicitud, por favor intenta más tarde';
 $lang['GEN_FOTTER_RIGHTS'] = '© Todos los derechos reservados. Produbanco.';
 $lang['GEN_CONTACTS_TITLE'] = 'Líneas de atención a nivel nacional';
+$lang['GEN_OTP_INCORRECT'] = 'Los datos ingresados son incorrectos.';
