540ac9f78a72e7fee280530ec53cfbb314220dbd
ylopez <ylopez@novopayment.com>
2022-08-03 10:05:51 -0500

    Incorpora configuración de imágenes.

---
+++ build/application/language/global/images_coop_lang.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+//GENERAL IMAGES
+$lang['IMG_FAVICON'] = 'favicon-coop';
+$lang['IMG_SIGNIN_LOGO_WIDGET'] = 'img-logo.svg';
+// NETWORKS
+$lang['IMG_FOTTER_NETWORKS_IMG'] = [
+	'facebook' => 'facebook.png',
+	'twitter' => 'twitter.png',
+	'instagram' => 'instagram.png',
+	'linkedin' => 'linkedin.png',
+];
