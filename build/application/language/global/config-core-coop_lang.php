f8a43c4de041fee8a02768c6273cdd6d1a0ab360
ylopez <ylopez@novopayment.com>
2022-08-03 10:05:05 -0500

    Incorpora configuración

---
+++ build/application/language/global/config-core-coop_lang.php
@@ -0,0 +1,43 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+//VALIDATE FORMS
+$lang['CONF_VALID_POSITION'] = 'right';
+//VALIDATE IP
+$lang['CONF_VALIDATE_IP'] = 'OFF';
+//CARD STYLES
+$lang['CONF_CARD_COLOR'] = 'text';
+//SIGNIN
+$lang['CONF_WIDGET_REST_COUNTRY'] = 'ON';
+//FOOTER
+$lang['CONF_FOOTER_NETWORKS'] = 'ON';
+$lang['CONF_FOOTER_LOGO'] = 'OFF';
+$lang['CONF_FOOTER_MARK'] = 'ON';
+//EXTERNAL LINKS
+$lang['CONF_FOTTER_NETWORKS_LINK'] = [
+	'facebook' => 'https://www.facebook.com/BCCoopcentral',
+	'twitter' => 'https://www.twitter.com/BcoCoopcentral',
+	'instagram' => 'https://www.instagram.com/bccoopcentral/?hl=es-la',
+	'linkedin' => 'https://www.linkedin.com/company/banco-cooperativo-coopcentral',
+];
+//USER IDENTITY
+$lang['CONF_SECRET_KEY'] = 'OFF';
+$lang['CONF_CONTACTS'] = [
+	'XXX' => '(XX) XXXX XXXX',
+];
+//SIGNUP-PROFILE
+$lang['CONF_UPDATE_USER'] = 'ON';
+$lang['CONF_UPDATE_NAME'] = 'OFF';
+$lang['CONF_LANDLINE'] = 'OFF';
+$lang['CONF_OTHER_PHONE'] = 'OFF';
+$lang['CONF_PROFESSION'] = 'OFF';
+$lang['CONF_CONTAC_DATA'] = 'OFF';
+$lang['CONF_CHECK_NOTI_EMAIL'] = 'OFF';
+$lang['CONF_CHECK_NOTI_SMS'] = 'OFF';
+$lang['CONF_LOAD_DOCS'] = 'ON';
+$lang['CONF_LOAD_DOC_F_ID'] = 'ON';
+$lang['CONF_LOAD_DOC_B_ID'] = 'ON';
+//REGEX
+$lang['CONF_REGEX_NICKNAME'] = '^([a-z]{2}[a-z0-9_]{4,14})$';
+$lang['CONF_REGEX_NICKNAME_PROFILE'] = '^([a-z]{2}[a-z0-9 .,+-_@*]{4,14})$';
+//NAVBAR STYLES
+$lang['CONF_PADDING_LOGO'] = 'OFF';
