bd84c12d5413530981b1c2cc9f69ac08e5f8425d
ylopez <ylopez@novopayment.com>
2022-08-23 12:03:05 -0500

    Ajusta customer para pruebas.

--- build/application/config/config-coop.php
+++ build/application/config/config-coop.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$config['customer'] = 'coop';
+$config['customer'] = 'Co';
 $config['customer-uri'] = 'coop';
 $config['base_url']	= BASE_URL.'/'.$config['customer-uri'].'/';
 $config['language']	= BASE_LANGUAGE.'-'.$config['customer-uri'];
