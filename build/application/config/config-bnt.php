719ffdd4887e37ed5952df7766de546959cf4aa7
ylopez <ylopez@novopayment.com>
2022-09-14 12:48:29 -0500

    Incopora varible client_style config bnt

--- build/application/config/config-bnt.php
+++ build/application/config/config-bnt.php
@@ -2,5 +2,6 @@
 defined('BASEPATH') OR exit('No direct script access allowed');
 $config['customer']='Mx-Bn';
 $config['customer-uri'] = 'bnt';
+$config['client_style'] = $config['customer-uri'];
 $config['base_url']	= BASE_URL.'/'.$config['customer-uri'].'/';
 $config['language']	= BASE_LANGUAGE.'-'.$config['customer-uri'];
