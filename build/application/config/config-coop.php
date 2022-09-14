cb2a037d04d5a3ef2a6b2066a8eb0df96ac07b72
ylopez <ylopez@novopayment.com>
2022-09-14 12:50:33 -0500

    Incopora variable client_style config coop

--- build/application/config/config-coop.php
+++ build/application/config/config-coop.php
@@ -1,6 +1,7 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-$config['customer'] = 'Co';
+$config['customer'] = 'coop';
 $config['customer-uri'] = 'coop';
+$config['client_style'] = $config['customer-uri'];
 $config['base_url']	= BASE_URL.'/'.$config['customer-uri'].'/';
 $config['language']	= BASE_LANGUAGE.'-'.$config['customer-uri'];
