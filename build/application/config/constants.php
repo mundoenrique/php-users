1a9bcadcc95c70f410eab8dd71af062da406b14e
Enrique Peñaloza <epenaloza@novopayment.com>
2022-09-20 07:38:34 -0400

    Actualiza codeigniter

--- build/application/config/constants.php
+++ build/application/config/constants.php
@@ -211,4 +211,3 @@ defined('ARGON2_SALT')					OR define('ARGON2_SALT', $_SERVER['ARGON2_SALT']);
 defined('KEY_API')	OR define('KEY_API', $_SERVER['KEY_API']);
 
 unset($uriSegments, $proxyIps, $timeZone, $baseLanguage, $errorController);
-
