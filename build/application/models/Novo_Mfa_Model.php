538fe3d7ac8b074f9e136447cd7c8d33a584aa7e
Enrique Peñaloza <epenaloza@novopayment.com>
2022-09-21 00:34:41 -0400

    Llamado para activación secret token

--- build/application/models/Novo_Mfa_Model.php
+++ build/application/models/Novo_Mfa_Model.php
@@ -22,7 +22,7 @@ class Novo_Mfa_Model extends NOVO_Model {
 	{
 		log_message('INFO', 'NOVO Mfa Model: Mfa GenerateSecretToken Method Initialized');
 
-		$this->dataRequest->uri = 'mfa/secrettoken/generate';
+		$this->dataRequest->uri = 'secret-token/generate';
 		$this->dataRequest->requestBody = [
 			'username' => $this->session->userName,
 			'authenticationChannel' => $dataRequest->channel
@@ -30,41 +30,13 @@ class Novo_Mfa_Model extends NOVO_Model {
 
 		$response = $this->sendToCoreServices('callWs_GenerateSecretToken');
 
-		switch ($dataRequest->channel) {
-			case lang('CONF_MFA_CHANNEL_APP'):
-				$response = json_decode('{"code":"200","datetime":"2022-08-02T15:33:07.154Z[UTC]","message":"Process Ok","data":{"appName":"Conexion personas","qrCode":"iVBORw0KGgoAAAANSUhEUgAAAMgAAADIAQAAAACFI5MzAAAB8ElEQVR4Xu2XUWoDMQxEDb6WQVc36FoL7rzZbWgK/ZOghZolIX4Gj6SxvBnnpzG+T7zGP/n1ZI8xzxXX3POK3GN6op6knj3PjrOZDE90kD3zVrCGdXiih2jvtUKhDtFGcs2Tye5K8LuCQqJHoQ7Dxecro6UEQ+TX8eadOuKhGKVCE/fH7fhacntc9Vp4ZHlEA8kdl9wRhHzziYpy4sIdpvSl0rHyVlBL1B4UbsgeUqDZKT3ZQZRR5TQ4VRorvLSePNZAgSpGu9iPglrCkV2yhoQEaU3W9hD2Vu/DIJGX+F25WnJpb7UIErmd0ZeCWpIunvy+cL4ojmwgYhwr7YyGIL3RQhTmcsU24VK+HuIWIap7yQklxQ1EzYFM2vHqTMONvYGoiWNGKVCQMqMEPZHWEjySPk/Xff0Z1xPNUjJXT62JlM4WcvFGd+h+EzmJMTuIDhZneGBDJVMlbCGEOdGxWUCYXQTnJfeSvK68urE3EOrE47LpxhifN3otSX5oa+0cyEj6YAuJJ6tooS99nqxiokGTWCzwKR6gcsLYinEK24i8rjQQDMh/iBFw3GgrlhP7I/HjZokVnA7iX8fbct2ad5HkbE1ftRjyibSeqCu5s/JFxB0EyNaUMN1h16OglNgh4W1xvo7yqyOVkh/GP/mr5AMhn0ZEpGg3rwAAAABJRU5ErkJggg==","qrCodeUrl":"otpauth://totp/Conexion%20personas:Novopayment?secret=EFN4FB227APRY5YW&issuer=Conexion+personas&algorithm=SHA1&digits=6&period=30","secretToken":"EFN4FB227APRY5YW"}}');
-			break;
-			default:
-				$response = json_decode('{"code":"200","message":"-----------Email-------------","datetime":"2012-10-01T09:45:00.000+02:00","data":""}');
-			break;
-		}
-		log_message('INFO', '****NOVO Mfa PRUEBA RESPONSE*****'.json_encode($response));
-
-		switch ($response->code) {
-			case 200:
-				if ($dataRequest->sendResendToken) {
-					$this->response->code = 0;
-				} else {
-					$this->response->code = 2;
-					$this->response->title = lang('GEN_MENU_TWO_FACTOR_ENABLEMENT');
-					$this->response->icon = lang('CONF_ICON_SUCCESS');
-					$this->response->msg =  novoLang(lang('MFA_TWO_FACTOR_RESEND_CODE'), $this->session->maskMail);
-					$this->response->modalBtn['btn1']['text'] = lang('GEN_BTN_ACCEPT');
-					$this->response->modalBtn['btn1']['action'] = 'destroy';
-				}
-			break;
-			default:
-			  $this->response->code = 3;
-				$this->response->title = lang('GEN_MENU_TWO_FACTOR_ENABLEMENT');
-				$this->response->icon = lang('CONF_ICON_INFO');
-				$this->response->msg = lang('GEN_SYSTEM_MESSAGE');
-				$this->response->modalBtn['btn1']['link'] = 'two-factor-enablement';
-				$this->response->modalBtn['btn1']['text'] = lang('GEN_BTN_ACCEPT');
-				$this->response->modalBtn['btn1']['action'] = 'redirect';
-			break;
+		switch ($this->isResponseRc) {
+			case 0:
+				$this->response->code = 0;
+				$this->response->data = $response->info->data;
+				break;
 		}
 
-		$this->response->data = $response->data;
 		return $this->responseToTheView('callWs_GenerateSecretToken');
 	}
 
