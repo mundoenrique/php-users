f9b88a0874ec26094c3c047a0d9a27ccb676b305
Enrique Peñaloza <epenaloza@novopayment.com>
2023-03-15 12:49:40 -0400

    Ajusta menú

--- build/application/language/global/settings_ve_lang.php
+++ build/application/language/global/settings_ve_lang.php
@@ -10,10 +10,10 @@ $lang['SETT_CONTACTS'] = [
 	'0501-PLATA-00' => '(0501-75282-00)',
 ];
 $lang['SETT_BTN_LANG'] = 'ON';
-$lang['SETT_TRANSFERS'] = 'OFF';
+$lang['SETT_TRANSFERS'] = 'ON';
 $lang['SETT_MOBILE_PAYMENT'] = 'ON';
 $lang['SETT_BETWEEN_CARDS'] = 'ON';
-$lang['SETT_BANKS'] = 'ON';
+$lang['SETT_BANKS'] = 'OFF';
 $lang['SETT_SERVICES'] = 'OFF';
 $lang['SETT_CUSTOMER_SUPPORT'] = 'OFF';
 $lang['SETT_SIGNIN_IMG'] = 'ON';
