a42b18ff115332d213bbeb63996dc64d07d78d4d
ylopez <ylopez@novopayment.com>
2023-05-19 11:38:37 -0500

    Desactiva notificación sms

--- build/application/language/global/settings_coop_lang.php
+++ build/application/language/global/settings_coop_lang.php
@@ -21,4 +21,5 @@ $lang['SETT_CONTACTS'] = [
 $lang['SETT_PADDING_LOGO'] = 'OFF';
 $lang['SETT_FRANCHISE_LOGO'] = 'OFF';
 $lang['SETT_NUMERIC_DOCUMENT_ID'] = ['E','C','F','N','U','T'];
+$lang['SETT_CHECK_NOTI_SMS'] = 'OFF';
 $lang['SETT_LINK_SERVICE_RECOVER_ACCESS'] = 'AccessRecoverOTP';
