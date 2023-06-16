7ba9cc670af0ada380fd9a460109b3584bcc2aa9
Enrique Peñaloza <epenaloza@novopayment.com>
2023-06-16 08:57:09 -0400

    Cambio en selección de Base de datos

--- build/application/helpers/novo_toolset_helper.php
+++ build/application/helpers/novo_toolset_helper.php
@@ -64,18 +64,9 @@ if (!function_exists('arrayTrim')) {
 
 if(!function_exists('dbSearch')) {
 	function dbSearch($uri) {
-		$clients = explode(',', ACCESS_URL);
-		array_walk($clients, 'arrayTrim');
-
-		if (($pos = array_search('default', $clients)) !== FALSE) {
-			unset($clients[$pos]);
-		}
-
-		if (($pos = array_search('pichincha', $clients)) !== FALSE) {
-			unset($clients[$pos]);
-		}
-
-		$dbName = in_array($uri, $clients) ? $uri : 'alpha';
+		$CI = &get_instance();
+		$defaultBd = 'alpha';
+		$dbName = DB_VERIFY ? $CI->config->item('client_db')[$uri] ?? $defaultBd : $defaultBd;
 
 		return 'cpo_' . $dbName;
 	}
@@ -327,8 +318,8 @@ if (!function_exists('uriRedirect')) {
 
 if (! function_exists('tenantSameSettings')) {
 	function tenantSameSettings($customer) {
-		$pattern = ['/bog/'];
-		$replace = ['bdb'];
+		$pattern = ['/bog/', '/per/', '/usd/'];
+		$replace = ['bdb', 'pe', 'us'];
 		$customer = preg_replace($pattern, $replace, $customer);
 
 		return $customer;
