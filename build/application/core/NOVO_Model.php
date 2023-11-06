e6ab5cf0ffd572ee2d6976b3b910933e0ff322dd
Enrique Peñaloza <epenaloza@novopayment.com>
2023-11-06 11:30:12 -0400

    Formato de archivo

--- build/application/core/NOVO_Model.php
+++ build/application/core/NOVO_Model.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 /**
  * Clase Modelo de Conexión Personas Online (CEO)
  *
@@ -10,201 +10,202 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  * @author J. Enrique Peñaloza Piñero
  * @date May 16th, 2020
  */
-class NOVO_Model extends CI_Model {
-	public $dataAccessLog;
-	public $accessLog;
-	public $customer;
-	public $customerUri;
-	public $customerFiles;
-	public $dataRequest;
-	public $userName;
-	public $keyId;
-	public $token;
-	public $isResponseRc;
-	public $response;
+class NOVO_Model extends CI_Model
+{
+  public $dataAccessLog;
+  public $accessLog;
+  public $customer;
+  public $customerUri;
+  public $customerFiles;
+  public $dataRequest;
+  public $userName;
+  public $keyId;
+  public $token;
+  public $isResponseRc;
+  public $response;
 
-	public function __construct()
-	{
-		parent:: __construct();
-		writeLog('INFO', 'Model Class Initialized');
+  public function __construct()
+  {
+    parent::__construct();
+    writeLog('INFO', 'Model Class Initialized');
 
-		$this->dataAccessLog = new stdClass();
-		$this->customer = $this->session->customerSess ?? $this->config->item('customer');
-		$this->customerUri = $this->session->customerUri ?? $this->config->item('customer_uri');
-		$this->customerFiles = $this->config->item('customer_files');
-		$this->dataRequest = new stdClass();
-		$this->userName = $this->session->userName;
-		$this->keyId = $this->session->userName ?? 'CPONLINE';
-		$this->token = $this->session->token ?? '';
-		$this->response = new stdClass();
-		$this->response->code = lang('SETT_DEFAULT_CODE');
-		$this->response->icon = lang('SETT_ICON_WARNING');
-		$this->response->title = lang('GEN_SYSTEM_NAME');
-		$this->response->msg = '';
-		$this->response->data = new stdClass();
-	}
-	/**
-	 * @info Método para comunicación con el servicio
-	 * @author J. Enrique Peñaloza Piñero.
-	 * @date May 16th, 2020
-	 */
-	public function sendToWebServices($model)
-	{
-		writeLog('INFO', 'Model: sendToWebServices Method Initialized');
+    $this->dataAccessLog = new stdClass();
+    $this->customer = $this->session->customerSess ?? $this->config->item('customer');
+    $this->customerUri = $this->session->customerUri ?? $this->config->item('customer_uri');
+    $this->customerFiles = $this->config->item('customer_files');
+    $this->dataRequest = new stdClass();
+    $this->userName = $this->session->userName;
+    $this->keyId = $this->session->userName ?? 'CPONLINE';
+    $this->token = $this->session->token ?? '';
+    $this->response = new stdClass();
+    $this->response->code = lang('SETT_DEFAULT_CODE');
+    $this->response->icon = lang('SETT_ICON_WARNING');
+    $this->response->title = lang('GEN_SYSTEM_NAME');
+    $this->response->msg = '';
+    $this->response->data = new stdClass();
+  }
+  /**
+   * @info Método para comunicación con el servicio
+   * @author J. Enrique Peñaloza Piñero.
+   * @date May 16th, 2020
+   */
+  public function sendToWebServices($model)
+  {
+    writeLog('INFO', 'Model: sendToWebServices Method Initialized');
 
-		$request = [];
-		$this->accessLog = accessLog($this->dataAccessLog);
+    $request = [];
+    $this->accessLog = accessLog($this->dataAccessLog);
 
-		if ($this->session->has_userdata('enterpriseCod') && $this->session->enterpriseCod !== '') {
-			$this->dataRequest->acCodCia = $this->session->enterpriseCod;
-		}
+    if ($this->session->has_userdata('enterpriseCod') && $this->session->enterpriseCod !== '') {
+      $this->dataRequest->acCodCia = $this->session->enterpriseCod;
+    }
 
-		$this->dataRequest->pais = $this->dataRequest->pais ?? $this->customer;
-		$this->dataRequest->token = $this->token;
-		$this->dataRequest->logAccesoObject = $this->accessLog;
-		$request['data'] = $this->dataRequest;
-		$request['pais'] = $this->dataRequest->pais;
-		$request['keyId'] = $this->keyId;
-		$dataRequest = json_encode($this->dataRequest, JSON_UNESCAPED_UNICODE);
+    $this->dataRequest->pais = $this->dataRequest->pais ?? $this->customer;
+    $this->dataRequest->token = $this->token;
+    $this->dataRequest->logAccesoObject = $this->accessLog;
+    $request['data'] = $this->dataRequest;
+    $request['pais'] = $this->dataRequest->pais;
+    $request['keyId'] = $this->keyId;
+    $dataRequest = json_encode($this->dataRequest, JSON_UNESCAPED_UNICODE);
 
-		writeLog('DEBUG', 'WEB SERVICES REQUEST ' . $model . ': ' . json_encode($request, JSON_UNESCAPED_UNICODE));
+    writeLog('DEBUG', 'WEB SERVICES REQUEST ' . $model . ': ' . json_encode($request, JSON_UNESCAPED_UNICODE));
 
-		$encryptRequest = $this->encrypt_decrypt->encryptWebServices($dataRequest);
-		$request['data'] = $encryptRequest;
-		$encryptResponse = $this->connect_services_apis->connectWebServices($request);
-		$response = $this->encrypt_decrypt->decryptWebServices($encryptResponse);
-		$response = handleResponseServer($response);
-		$logResponse = handleLogResponse($response);
+    $encryptRequest = $this->encrypt_decrypt->encryptWebServices($dataRequest);
+    $request['data'] = $encryptRequest;
+    $encryptResponse = $this->connect_services_apis->connectWebServices($request);
+    $response = $this->encrypt_decrypt->decryptWebServices($encryptResponse);
+    $response = handleResponseServer($response);
+    $logResponse = handleLogResponse($response);
 
-		writeLog('DEBUG', 'WEB SERVICES RESPONSE ' . $model . ': ' . json_encode($logResponse, JSON_UNESCAPED_UNICODE));
+    writeLog('DEBUG', 'WEB SERVICES RESPONSE ' . $model . ': ' . json_encode($logResponse, JSON_UNESCAPED_UNICODE));
 
-		unset($logResponse);
+    unset($logResponse);
 
-		return $this->makeAnswer($response);
-	}
+    return $this->makeAnswer($response);
+  }
 
-	/**
-	 * @info Método para comunicación con el microservicio
-	 * @author Luis Molina.
-	 * @date MJun 16th, 2022
-	 */
-	public function sendToMfaServices($model)
-	{
-		writeLog('INFO', 'Model: sendToMfaServices Method Initialized');
+  /**
+   * @info Método para comunicación con el microservicio
+   * @author Luis Molina.
+   * @date MJun 16th, 2022
+   */
+  public function sendToMfaServices($model)
+  {
+    writeLog('INFO', 'Model: sendToMfaServices Method Initialized');
 
-		$response = NULL;
-		$this->dataRequest->requestBody = json_encode($this->dataRequest->requestBody, JSON_UNESCAPED_UNICODE);
+    $response = NULL;
+    $this->dataRequest->requestBody = json_encode($this->dataRequest->requestBody, JSON_UNESCAPED_UNICODE);
 
-		writeLog('DEBUG', 'MFA SERVICES REQUEST ' . $model . ': ' . $this->dataRequest->requestBody);
+    writeLog('DEBUG', 'MFA SERVICES REQUEST ' . $model . ': ' . $this->dataRequest->requestBody);
 
-		if (API_GEE_WAY) {
-			$this->dataRequest->requestBody = $this->encrypt_decrypt->encryptCoreServices($this->dataRequest->requestBody);
-		}
+    if (API_GEE_WAY) {
+      $this->dataRequest->requestBody = $this->encrypt_decrypt->encryptCoreServices($this->dataRequest->requestBody);
+    }
 
-		$response = $this->connect_services_apis->connectMfaServices($this->dataRequest);
+    $response = $this->connect_services_apis->connectMfaServices($this->dataRequest);
 
-		if (API_GEE_WAY && $response->data) {
-			$response = $this->encrypt_decrypt->decryptCoreServices($response->data);
-		}
+    if (API_GEE_WAY && $response->data) {
+      $response = $this->encrypt_decrypt->decryptCoreServices($response->data);
+    }
 
-		writeLog('DEBUG', 'MFA SERVICES RESPONSE COMPLETE ' . $model . ': '
-			. json_encode($response, JSON_UNESCAPED_UNICODE));
+    writeLog('DEBUG', 'MFA SERVICES RESPONSE COMPLETE ' . $model . ': '
+      . json_encode($response, JSON_UNESCAPED_UNICODE));
 
-		return $this->makeAnswer($response);
-	}
+    return $this->makeAnswer($response);
+  }
 
-	/**
-	 * @info Método armar la respuesta a los modelos
-	 * @author J. Enrique Peñaloza Piñero
-	 * @date May 16th, 2020
-	 */
-	protected function makeAnswer($responseModel)
-	{
-		writeLog('INFO', 'Model: makeAnswer Method Initialized');
+  /**
+   * @info Método armar la respuesta a los modelos
+   * @author J. Enrique Peñaloza Piñero
+   * @date May 16th, 2020
+   */
+  protected function makeAnswer($responseModel)
+  {
+    writeLog('INFO', 'Model: makeAnswer Method Initialized');
 
-		$this->isResponseRc = (int) $responseModel->responseCode;
+    $this->isResponseRc = (int) $responseModel->responseCode;
 
-		$linkredirect = uriRedirect();
-		$arrayResponse = [
-			'btn1'=> [
-				'text'=> lang('GEN_BTN_ACCEPT'),
-				'link'=> $linkredirect,
-				'action'=> 'redirect'
-			]
-		];
+    $linkredirect = uriRedirect();
+    $arrayResponse = [
+      'btn1' => [
+        'text' => lang('GEN_BTN_ACCEPT'),
+        'link' => $linkredirect,
+        'action' => 'redirect'
+      ]
+    ];
 
-		switch($this->isResponseRc) {
-			case -29:
-			case -35:
-			case -61:
-				$this->response->msg = lang('GEN_DUPLICATED_SESSION');
-				clearSessionsVars();
-			break;
-			case 502:
-				$this->response->msg = lang('GEN_SYSTEM_MESSAGE');
-				clearSessionsVars();
-			break;
-			case 504:
-				$this->response->msg = lang('GEN_TIMEOUT');
-			break;
-			default:
-				$this->response->icon = lang('SETT_ICON_DANGER');
-				$this->response->msg = lang('GEN_SYSTEM_MESSAGE');
-		}
+    switch ($this->isResponseRc) {
+      case -29:
+      case -35:
+      case -61:
+        $this->response->msg = lang('GEN_DUPLICATED_SESSION');
+        clearSessionsVars();
+        break;
+      case 502:
+        $this->response->msg = lang('GEN_SYSTEM_MESSAGE');
+        clearSessionsVars();
+        break;
+      case 504:
+        $this->response->msg = lang('GEN_TIMEOUT');
+        break;
+      default:
+        $this->response->icon = lang('SETT_ICON_DANGER');
+        $this->response->msg = lang('GEN_SYSTEM_MESSAGE');
+    }
 
-		$this->response->modalBtn = $arrayResponse;
-		$this->response->msg = $this->isResponseRc === 0 ? lang('GEN_SUCCESS_RESPONSE') : $this->response->msg;
+    $this->response->modalBtn = $arrayResponse;
+    $this->response->msg = $this->isResponseRc === 0 ? lang('GEN_SUCCESS_RESPONSE') : $this->response->msg;
 
-		return $responseModel->data;
-	}
-	/**
-	 * @info Método enviar el resultado de la consulta a la vista
-	 * @author J. Enrique Peñaloza Piñero.
-	 * @date May 16th, 2020
-	 */
-	public function responseToTheView($model)
-	{
-		writeLog('INFO', 'Model: responseToView Method Initialized');
-		$responsetoView = new stdClass();
+    return $responseModel->data;
+  }
+  /**
+   * @info Método enviar el resultado de la consulta a la vista
+   * @author J. Enrique Peñaloza Piñero.
+   * @date May 16th, 2020
+   */
+  public function responseToTheView($model)
+  {
+    writeLog('INFO', 'Model: responseToView Method Initialized');
+    $responsetoView = new stdClass();
 
-		foreach ($this->response AS $pos => $response) {
-			if (isset($response->file)) {
-				continue;
-			}
+    foreach ($this->response as $pos => $response) {
+      if (isset($response->file)) {
+        continue;
+      }
 
-			$responsetoView->$pos = $response;
+      $responsetoView->$pos = $response;
 
-			if (!empty($response->profileData->imagesLoaded)) {
-				$responsetoView->data->profileData->imagesLoaded = 'cypher image';
-			}
-		}
+      if (!empty($response->profileData->imagesLoaded)) {
+        $responsetoView->data->profileData->imagesLoaded = 'cypher image';
+      }
+    }
 
-		writeLog('DEBUG', 'RESULT ' . $model . ' SENT TO THE VIEW '.json_encode($responsetoView, JSON_UNESCAPED_UNICODE));
+    writeLog('DEBUG', 'RESULT ' . $model . ' SENT TO THE VIEW ' . json_encode($responsetoView, JSON_UNESCAPED_UNICODE));
 
-		unset($responsetoView);
+    unset($responsetoView);
 
-		return $this->response;
-	}
-	/**
-	 * @info Método para validar la carga de imagenes del usurio
-	 * @author J. Enrique Peñaloza Piñero.
-	 * @date July 13th, 2021
-	 */
-	public function checkImageUpload()
-	{
-		writeLog('INFO', 'Model: checkImageUpload Method Initialized');
+    return $this->response;
+  }
+  /**
+   * @info Método para validar la carga de imagenes del usurio
+   * @author J. Enrique Peñaloza Piñero.
+   * @date July 13th, 2021
+   */
+  public function checkImageUpload()
+  {
+    writeLog('INFO', 'Model: checkImageUpload Method Initialized');
 
-		if($this->session->missingImages) {
-			$this->response->code = 3;
-			$this->response->title = lang('GEN_TITLE_IMPORTANT');
-			$this->response->icon = lang('SETT_ICON_INFO');
-			$this->response->msg = lang('GEN_MISSING_IMAGES');
-			$this->response->modalBtn['btn1']['text'] = lang('GEN_BTN_YES');
-			$this->response->modalBtn['btn1']['link'] = lang('SETT_LINK_USER_PROFILE');
-			$this->response->modalBtn['btn2']['text'] = lang('GEN_BTN_NO');
-			$this->response->modalBtn['btn2']['action'] = 'destroy';
+    if ($this->session->missingImages) {
+      $this->response->code = 3;
+      $this->response->title = lang('GEN_TITLE_IMPORTANT');
+      $this->response->icon = lang('SETT_ICON_INFO');
+      $this->response->msg = lang('GEN_MISSING_IMAGES');
+      $this->response->modalBtn['btn1']['text'] = lang('GEN_BTN_YES');
+      $this->response->modalBtn['btn1']['link'] = lang('SETT_LINK_USER_PROFILE');
+      $this->response->modalBtn['btn2']['text'] = lang('GEN_BTN_NO');
+      $this->response->modalBtn['btn2']['action'] = 'destroy';
 
-			$this->session->set_userdata('missingImages', FALSE);
-		}
-	}
+      $this->session->set_userdata('missingImages', FALSE);
+    }
+  }
 }
--- build/application/helpers/novo_server_helper.php
+++ build/application/helpers/novo_server_helper.php
@@ -1,5 +1,5 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 /**
  * NOVOPAYMENT server Helpers
  *
@@ -10,46 +10,47 @@ defined('BASEPATH') OR exit('No direct script access allowed');
  */
 
 if (!function_exists('writeLog')) {
-	function writeLog($level, $message) {
-		$CI =& get_instance();
-		$ip = $CI->input->ip_address();
-		$level = mb_strtoupper($level);
+  function writeLog($level, $message)
+  {
+    $CI = &get_instance();
+    $ip = $CI->input->ip_address();
+    $level = mb_strtoupper($level);
 
-		if ($level !== 'INFO') {
-			$customer = $CI->session->customerSess ?? $CI->config->item('customer');
-			$isLogUser = $CI->session->has_userdata('logUser');
-			$logUser = $CI->session->logUser ?? date('U') . '-';
-			list($date, $user) = explode('-', $logUser);
-			$reqUser = $user;
+    if ($level !== 'INFO') {
+      $customer = $CI->session->customerSess ?? $CI->config->item('customer');
+      $isLogUser = $CI->session->has_userdata('logUser');
+      $logUser = $CI->session->logUser ?? date('U') . '-';
+      list($date, $user) = explode('-', $logUser);
+      $reqUser = $user;
 
-			if ($CI->session->has_userdata('userName')) {
-				$reqUser = $CI->session->userName;
-			} elseif($CI->input->get_post('userName') !== NULL) {
-				$reqUser = mb_strtoupper($CI->input->get_post('userName'));
-			} elseif($CI->input->get_post('idNumber') !== NULL) {
-				$reqUser = mb_strtoupper($CI->input->get_post('idNumber'));
-			} elseif($CI->input->get_post('documentId') !== NULL) {
-				$reqUser = mb_strtoupper($CI->input->get_post('documentId'));
-			}
+      if ($CI->session->has_userdata('userName')) {
+        $reqUser = $CI->session->userName;
+      } elseif ($CI->input->get_post('userName') !== NULL) {
+        $reqUser = mb_strtoupper($CI->input->get_post('userName'));
+      } elseif ($CI->input->get_post('idNumber') !== NULL) {
+        $reqUser = mb_strtoupper($CI->input->get_post('idNumber'));
+      } elseif ($CI->input->get_post('documentId') !== NULL) {
+        $reqUser = mb_strtoupper($CI->input->get_post('documentId'));
+      }
 
-			if ($isLogUser === NULL || $user !== $reqUser) {
-				$logUser = $date . '-' . $reqUser;
-				$CI->session->set_userdata('logUser', $logUser);
-			}
+      if ($isLogUser === NULL || $user !== $reqUser) {
+        $logUser = $date . '-' . $reqUser;
+        $CI->session->set_userdata('logUser', $logUser);
+      }
 
-			$message = novoLang('NOVO [%s] IP: %s, CUSTOMER: %s, %s', [$logUser, $ip, $customer, $message]);
+      $message = novoLang('NOVO [%s] IP: %s, CUSTOMER: %s, %s', [$logUser, $ip, $customer, $message]);
+    } else {
+      $message = novoLang('NOVO %s', $message);
+    }
 
-		} else {
-			$message = novoLang('NOVO %s', $message);
-		}
-
-		log_message($level, $message);
-	}
+    log_message($level, $message);
+  }
 }
 
 // generar uuIdV4
 if (!function_exists('uuIdV4Generate')) {
-	function uuIdV4Generate() {
+  function uuIdV4Generate()
+  {
     // Generate 16 bytes (128 bits) of random data or use the data passed into the function.
     $data = random_bytes(16);
     assert(strlen($data) == 16);
@@ -61,118 +62,120 @@ if (!function_exists('uuIdV4Generate')) {
 
     // Output the 36 character UUID.
     return vsprintf('%s%s-%s-%s-%s-%s%s%s', str_split(bin2hex($data), 4));
-	}
+  }
 }
 
 // Maneja respuesta del servicio
 if (!function_exists('responseServer')) {
-	function responseServer($responseServer) {
-		$code = explode('.', $responseServer->code);
+  function responseServer($responseServer)
+  {
+    $code = explode('.', $responseServer->code);
 
-		switch ($responseServer->HttpCode) {
-			case 200:
-				$responseServer->responseCode = 0;
-				break;
+    switch ($responseServer->HttpCode) {
+      case 200:
+        $responseServer->responseCode = 0;
+        break;
 
-			case 502:
-				$responseServer->responseCode = 502;
-				break;
+      case 502:
+        $responseServer->responseCode = 502;
+        break;
 
-			case 504:
-				$responseServer->responseCode = 504;
-				break;
+      case 504:
+        $responseServer->responseCode = 504;
+        break;
 
-			default:
-				$responseServer->responseCode = $code[2] ?? $code[0];
-		}
+      default:
+        $responseServer->responseCode = $code[2] ?? $code[0];
+    }
 
-		if ($responseServer->errorNo === 28) {
-			$responseServer->responseCode = 504;
-		}
+    if ($responseServer->errorNo === 28) {
+      $responseServer->responseCode = 504;
+    }
 
-		return $responseServer;
-	}
+    return $responseServer;
+  }
 }
 
 if (!function_exists('handleResponseServer')) {
-	function handleResponseServer($webServiceResp) {
-		if (isset($webServiceResp->data->rc)) {
-			$webServiceResp->responseCode = $webServiceResp->data->rc;
-		}
+  function handleResponseServer($webServiceResp)
+  {
+    if (isset($webServiceResp->data->rc)) {
+      $webServiceResp->responseCode = $webServiceResp->data->rc;
+    }
 
-		if (isset($webServiceResp->data->logAcceso)) {
-			$accessLog = json_decode($webServiceResp->data->logAcceso);
+    if (isset($webServiceResp->data->logAcceso)) {
+      $accessLog = json_decode($webServiceResp->data->logAcceso);
 
-			if (gettype($accessLog) === 'object') {
-				$webServiceResp->data->logAcceso = $accessLog;
-			}
-		}
+      if (gettype($accessLog) === 'object') {
+        $webServiceResp->data->logAcceso = $accessLog;
+      }
+    }
 
-		if (isset($webServiceResp->data->archivo)) {
-			$webServiceResp->binaryFile = 'success';
+    if (isset($webServiceResp->data->archivo)) {
+      $webServiceResp->binaryFile = 'success';
 
-			if (!is_array($webServiceResp->data->archivo)  || empty($webServiceResp->data->archivo)) {
-				$webServiceResp->binaryFile = 'No binary array';
-			}
-		}
+      if (!is_array($webServiceResp->data->archivo)  || empty($webServiceResp->data->archivo)) {
+        $webServiceResp->binaryFile = 'No binary array';
+      }
+    }
 
-		if (isset($webServiceResp->data->bean)) {
-			$bean = json_decode($webServiceResp->data->bean);
+    if (isset($webServiceResp->data->bean)) {
+      $bean = json_decode($webServiceResp->data->bean);
 
-			if (gettype($bean) === 'object' || gettype($bean) === 'array') {
-				$webServiceResp->data->bean = $bean;
-			}
+      if (gettype($bean) === 'object' || gettype($bean) === 'array') {
+        $webServiceResp->data->bean = $bean;
+      }
 
-			if (isset($webServiceResp->data->bean->archivo)) {
-				$webServiceResp->binaryFile = 'success';
+      if (isset($webServiceResp->data->bean->archivo)) {
+        $webServiceResp->binaryFile = 'success';
 
-				if (!is_array($webServiceResp->data->bean->archivo) || empty($webServiceResp->data->bean->archivo)) {
-					$webServiceResp->binaryFile = 'No binary array';
-				}
-			}
-		}
+        if (!is_array($webServiceResp->data->bean->archivo) || empty($webServiceResp->data->bean->archivo)) {
+          $webServiceResp->binaryFile = 'No binary array';
+        }
+      }
+    }
 
-		return $webServiceResp;
-	}
+    return $webServiceResp;
+  }
 }
 
 if (!function_exists('handleLogResponse')) {
-	function handleLogResponse($responseToLog) {
-		$logResponse = new stdClass();
+  function handleLogResponse($responseToLog)
+  {
+    $logResponse = new stdClass();
 
-		foreach ($responseToLog as $pos => $data) {
-			if ($pos === 'data' && (gettype($data) === 'object'  || gettype($data) === 'array')) {
-				$logResponse->data = new stdClass();
+    foreach ($responseToLog as $pos => $data) {
+      if ($pos === 'data' && (gettype($data) === 'object'  || gettype($data) === 'array')) {
+        $logResponse->data = new stdClass();
 
-				foreach ($data as $key => $value) {
-					if ($key === 'archivo') {
-						continue;
-					}
+        foreach ($data as $key => $value) {
+          if ($key === 'archivo') {
+            continue;
+          }
 
-					if ($key === 'bean' && (gettype($value) === 'object'  || gettype($value) === 'array')) {
-						$logResponse->data->bean = new stdClass();
+          if ($key === 'bean' && (gettype($value) === 'object'  || gettype($value) === 'array')) {
+            $logResponse->data->bean = new stdClass();
 
-						foreach ($value as $index => $content) {
-							if ($index === 'archivo') {
-								continue;
-							}
+            foreach ($value as $index => $content) {
+              if ($index === 'archivo') {
+                continue;
+              }
 
-							$logResponse->data->bean->$index = $content;
-						}
+              $logResponse->data->bean->$index = $content;
+            }
 
-						continue;
-					}
+            continue;
+          }
 
-					$logResponse->data->$key = $value;
-				}
+          $logResponse->data->$key = $value;
+        }
 
-				continue;
-			}
+        continue;
+      }
 
-			$logResponse->$pos = $data;
-		}
+      $logResponse->$pos = $data;
+    }
 
-		return $logResponse;
-	}
+    return $logResponse;
+  }
 }
-
