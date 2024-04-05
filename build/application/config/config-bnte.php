47d6645ff2462aed4e84b543447c8739ec2f4f2d
Enrique Peñaloza <epenaloza@novopayment.com>
2024-04-05 13:01:20 -0400

    tenant BNTE ajustes

--- build/application/config/config-bnte.php
+++ build/application/config/config-bnte.php
@@ -1,9 +1,9 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
+defined('BASEPATH') or exit('No direct script access allowed');
 $config['customer'] = 'Mx-Bn';
-$config['customer_uri'] = 'bnt';
-$config['customer_lang'] = $config['customer_uri'];
-$config['customer_style'] = $config['customer_uri'];
-$config['customer_files'] = $config['customer_uri'];
-$config['base_url']	= BASE_URL . '/' . $config['customer_uri'] . '/';
-$config['language']	= BASE_LANGUAGE . '-' . $config['customer_lang'];
+$config['customer_uri'] = 'bnte';
+$config['customer_lang'] = 'bnt';
+$config['customer_style'] = $config['customer_lang'];
+$config['customer_files'] = $config['customer_lang'];
+$config['base_url']  = BASE_URL . '/' . $config['customer_uri'] . '/';
+$config['language']  = BASE_LANGUAGE . '-' . $config['customer_lang'];
--- build/application/libraries/Connect_Services_Apis.php
+++ build/application/libraries/Connect_Services_Apis.php
@@ -6,211 +6,210 @@ defined('BASEPATH') or exit('No direct script access allowed');
  */
 class Connect_Services_Apis
 {
-	private $CI;
+  private $CI;
 
-	public function __construct()
-	{
-		writeLog('INFO', 'Connect_Services_Apis Library Class Initialized');
+  public function __construct()
+  {
+    writeLog('INFO', 'Connect_Services_Apis Library Class Initialized');
 
-		$this->CI =& get_instance();
-	}
+    $this->CI = &get_instance();
+  }
 
-	public function connectWebServices($request)
-	{
-		writeLog('INFO', 'Connect_Services_Apis: connectWebLogicservices Method Initialized');
+  public function connectWebServices($request)
+  {
+    writeLog('INFO', 'Connect_Services_Apis: connectWebLogicservices Method Initialized');
 
-		if (ENVIRONMENT === 'development') {
-			error_reporting(E_ALL & ~E_DEPRECATED);
-		}
+    if (ENVIRONMENT === 'development') {
+      error_reporting(E_ALL & ~E_DEPRECATED);
+    }
 
-		$subFix = '_' . strtoupper($this->CI->config->item('customer_uri'));
-		$wsUrl = $_SERVER['WS_URL'];
+    $subFix = '_' . strtoupper($this->CI->config->item('customer_uri'));
+    $wsUrl = $_SERVER['WS_URL'];
 
-		if (isset($_SERVER['WS_URL' . $subFix])) {
-			$wsUrl = $_SERVER['WS_URL' . $subFix];
-		}
+    if (isset($_SERVER['WS_URL' . $subFix])) {
+      $wsUrl = $_SERVER['WS_URL' . $subFix];
+    }
 
-		writeLog('DEBUG', 'REQUEST BY WEB SERVICE URL: ' . $wsUrl);
+    writeLog('DEBUG', $subFix . 'REQUEST BY WEB SERVICE URL: ' . $wsUrl);
 
-		$method = $request->method ?? 'POST';
-		unset($request->method);
-		$requestSerV = json_encode($request, JSON_UNESCAPED_UNICODE);
-		$curl = curl_init();
+    $method = $request->method ?? 'POST';
+    unset($request->method);
+    $requestSerV = json_encode($request, JSON_UNESCAPED_UNICODE);
+    $curl = curl_init();
 
-		curl_setopt_array($curl, [
-			CURLOPT_URL => $wsUrl,
-			CURLOPT_RETURNTRANSFER => TRUE,
-			CURLOPT_ENCODING => 'gzip, deflate, br',
-			CURLOPT_MAXREDIRS => 10,
-			CURLOPT_TIMEOUT => 58,
-			CURLOPT_FOLLOWLOCATION => TRUE,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => $method,
-			CURLOPT_HTTPHEADER => [
-				'Content-Type: text/plain',
-				'Content-Length: ' . strlen($requestSerV)
-			]
-		]);
+    curl_setopt_array($curl, [
+      CURLOPT_URL => $wsUrl,
+      CURLOPT_RETURNTRANSFER => TRUE,
+      CURLOPT_ENCODING => 'gzip, deflate, br',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 58,
+      CURLOPT_FOLLOWLOCATION => TRUE,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => $method,
+      CURLOPT_HTTPHEADER => [
+        'Content-Type: text/plain',
+        'Content-Length: ' . strlen($requestSerV)
+      ]
+    ]);
 
-		if ($method !== 'GET') {
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $requestSerV);
-		}
+    if ($method !== 'GET') {
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $requestSerV);
+    }
 
-		$curlResp = json_decode(curl_exec($curl));
-		$executionTime = curl_getinfo($curl, CURLINFO_TOTAL_TIME);
+    $curlResp = json_decode(curl_exec($curl));
+    $executionTime = curl_getinfo($curl, CURLINFO_TOTAL_TIME);
 
-		$response = new stdClass();
-		$response->HttpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-		$response->code = curl_getinfo($curl, CURLINFO_RESPONSE_CODE);
-		$response->data = isset($curlResp->data) ? $curlResp->data : NULL;
-		$response->errorNo = (int) curl_errno($curl);
-		$response->error = curl_error($curl);
+    $response = new stdClass();
+    $response->HttpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    $response->code = curl_getinfo($curl, CURLINFO_RESPONSE_CODE);
+    $response->data = isset($curlResp->data) ? $curlResp->data : NULL;
+    $response->errorNo = (int) curl_errno($curl);
+    $response->error = curl_error($curl);
 
-		if ($response->HttpCode !== 200 || !$curlResp) {
-			$response->code = lang('SETT_RC_DEFAULT');
-		}
+    if ($response->HttpCode !== 200 || !$curlResp) {
+      $response->code = lang('SETT_RC_DEFAULT');
+    }
 
-		curl_close($curl);
-		$executionTime = round($executionTime, 2, PHP_ROUND_HALF_UP);
+    curl_close($curl);
+    $executionTime = round($executionTime, 2, PHP_ROUND_HALF_UP);
 
-		writeLog('DEBUG', 'RESPONSE IN '. $executionTime . ' SEC, CURL HTTPCODE: ' . $response->HttpCode);
+    writeLog('DEBUG', 'RESPONSE IN ' . $executionTime . ' SEC, CURL HTTPCODE: ' . $response->HttpCode);
 
-		if ($response->errorNo !== 0) {
-			writeLog('ERROR', 'CURL ERROR NUMBER: ' . $response->errorNo . ', ERROR MESSAGE: ' . $response->error);
-		}
+    if ($response->errorNo !== 0) {
+      writeLog('ERROR', 'CURL ERROR NUMBER: ' . $response->errorNo . ', ERROR MESSAGE: ' . $response->error);
+    }
 
-		return responseServer($response);
-	}
+    return responseServer($response);
+  }
 
-	public function connectMfaServices($request)
-	{
-		writeLog('INFO', 'Connect_Services_Apis: connectMfaServices Method Initialized');
+  public function connectMfaServices($request)
+  {
+    writeLog('INFO', 'Connect_Services_Apis: connectMfaServices Method Initialized');
 
-		if (API_GEE_WAY && !$this->CI->session->tempdata('jwtOauth')) {
-			$this->getJwtOauthApiGee();
-		}
+    if (API_GEE_WAY && !$this->CI->session->tempdata('jwtOauth')) {
+      $this->getJwtOauthApiGee();
+    }
 
-		$urlMfaServ = URL_MFA_SERV . $request->uri;
-		$method = $request->method ?? 'POST';
-		unset($request->method);
-		$uuIdV4 = uuIdV4Generate();
+    $urlMfaServ = URL_MFA_SERV . $request->uri;
+    $method = $request->method ?? 'POST';
+    unset($request->method);
+    $uuIdV4 = uuIdV4Generate();
 
-		writeLog('DEBUG', 'REQUEST BY MFA SERVICE URL: ' . $urlMfaServ. ', UUID: '. $uuIdV4);
+    writeLog('DEBUG', 'REQUEST BY MFA SERVICE URL: ' . $urlMfaServ . ', UUID: ' . $uuIdV4);
 
-		$curl = curl_init();
+    $curl = curl_init();
 
-		curl_setopt_array($curl, [
-			CURLOPT_URL => $urlMfaServ,
-			CURLOPT_RETURNTRANSFER => TRUE,
-			CURLOPT_ENCODING => 'gzip, deflate, br',
-			CURLOPT_MAXREDIRS => 10,
-			CURLOPT_TIMEOUT => 58,
-			CURLOPT_FOLLOWLOCATION => TRUE,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => $method,
-			CURLOPT_HTTPHEADER => [
-				'Content-Type: application/json; charset=utf-8',
-				'accept: application/json; charset=utf-8',
-				'X-Request-Id: ' . $uuIdV4,
-				'X-Tenant-Id: pe-servitebca',
-			],
-		]);
+    curl_setopt_array($curl, [
+      CURLOPT_URL => $urlMfaServ,
+      CURLOPT_RETURNTRANSFER => TRUE,
+      CURLOPT_ENCODING => 'gzip, deflate, br',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 58,
+      CURLOPT_FOLLOWLOCATION => TRUE,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => $method,
+      CURLOPT_HTTPHEADER => [
+        'Content-Type: application/json; charset=utf-8',
+        'accept: application/json; charset=utf-8',
+        'X-Request-Id: ' . $uuIdV4,
+        'X-Tenant-Id: pe-servitebca',
+      ],
+    ]);
 
-		if ($method !== 'GET') {
-			curl_setopt($curl, CURLOPT_POSTFIELDS, $request->requestBody);
-		}
+    if ($method !== 'GET') {
+      curl_setopt($curl, CURLOPT_POSTFIELDS, $request->requestBody);
+    }
 
-		$curlResp = json_decode(curl_exec($curl));
-		$executionTime = curl_getinfo($curl, CURLINFO_TOTAL_TIME);
+    $curlResp = json_decode(curl_exec($curl));
+    $executionTime = curl_getinfo($curl, CURLINFO_TOTAL_TIME);
 
-		$response = new stdClass();
-		$response->HttpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-		$response->code = isset($curlResp->code) ? $curlResp->code : lang('SETT_RC_DEFAULT');
-		$response->datetime = isset($curlResp->datetime) ? $curlResp->datetime : '';
-		$response->message = isset($curlResp->message) ? $curlResp->message : '';
-		$response->data = isset($curlResp->data) ? $curlResp->data : NULL;
-		$response->error = curl_error($curl);
-		$response->errorNo = (int) curl_errno($curl);
+    $response = new stdClass();
+    $response->HttpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    $response->code = isset($curlResp->code) ? $curlResp->code : lang('SETT_RC_DEFAULT');
+    $response->datetime = isset($curlResp->datetime) ? $curlResp->datetime : '';
+    $response->message = isset($curlResp->message) ? $curlResp->message : '';
+    $response->data = isset($curlResp->data) ? $curlResp->data : NULL;
+    $response->error = curl_error($curl);
+    $response->errorNo = (int) curl_errno($curl);
 
-		curl_close($curl);
-		$executionTime = round($executionTime, 2, PHP_ROUND_HALF_UP);
+    curl_close($curl);
+    $executionTime = round($executionTime, 2, PHP_ROUND_HALF_UP);
 
-		writeLog('DEBUG', 'RESPONSE IN '. $executionTime . ' SEC, CURL HTTPCODE: ' . $response->HttpCode .
-			', SERVICE CODE: ' . $response->code . ' ' .$response->message);
+    writeLog('DEBUG', 'RESPONSE IN ' . $executionTime . ' SEC, CURL HTTPCODE: ' . $response->HttpCode .
+      ', SERVICE CODE: ' . $response->code . ' ' . $response->message);
 
-		if ($response->errorNo !== 0) {
-			writeLog('ERROR', 'CURL ERROR NUMBER: ' . $response->errorNo . ', ERROR MESSAGE: ' . $response->error);
-		}
+    if ($response->errorNo !== 0) {
+      writeLog('ERROR', 'CURL ERROR NUMBER: ' . $response->errorNo . ', ERROR MESSAGE: ' . $response->error);
+    }
 
-		return responseServer($response);
-	}
+    return responseServer($response);
+  }
 
-	public function getJwtOauthApiGee()
-	{
-		writeLog('INFO', 'Connect_Services_Apis: getJwtOauthApiGee Method Initialized');
+  public function getJwtOauthApiGee()
+  {
+    writeLog('INFO', 'Connect_Services_Apis: getJwtOauthApiGee Method Initialized');
 
-		$urlApiGeeHost = URL_APIGEE_OAUTH;
-		$clientIdApiGee = CLIENT_ID_APIGEE;
-		$ClientSecretApigee = CLIENT_SECRET_APIGEE;
+    $urlApiGeeHost = URL_APIGEE_OAUTH;
+    $clientIdApiGee = CLIENT_ID_APIGEE;
+    $ClientSecretApigee = CLIENT_SECRET_APIGEE;
 
-		writeLog('DEBUG', 'CORE_SERVICE URL: ' . $urlApiGeeHost);
+    writeLog('DEBUG', 'CORE_SERVICE URL: ' . $urlApiGeeHost);
 
-		$startReq = microtime(true);
-		$curl = curl_init();
+    $startReq = microtime(true);
+    $curl = curl_init();
 
-		curl_setopt_array($curl, [
-			CURLOPT_URL => $urlApiGeeHost,
-			CURLOPT_RETURNTRANSFER => TRUE,
-			CURLOPT_ENCODING => 'gzip, deflate, br',
-			CURLOPT_MAXREDIRS => 10,
-			CURLOPT_TIMEOUT => 58,
-			CURLOPT_FOLLOWLOCATION => TRUE,
-			CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-			CURLOPT_CUSTOMREQUEST => 'POST',
-			CURLOPT_HTTPHEADER => [
-				'Content-type: application/x-www-form-urlencoded; charset=utf-8',
-				'accept: application/json; charset=utf-8',
-			],
-			CURLOPT_POSTFIELDS => http_build_query([
-				'grant_type' => 'client_credentials',
-				'client_id' => $clientIdApiGee,
-				'client_secret' => $ClientSecretApigee
-			]),
-		]);
+    curl_setopt_array($curl, [
+      CURLOPT_URL => $urlApiGeeHost,
+      CURLOPT_RETURNTRANSFER => TRUE,
+      CURLOPT_ENCODING => 'gzip, deflate, br',
+      CURLOPT_MAXREDIRS => 10,
+      CURLOPT_TIMEOUT => 58,
+      CURLOPT_FOLLOWLOCATION => TRUE,
+      CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+      CURLOPT_CUSTOMREQUEST => 'POST',
+      CURLOPT_HTTPHEADER => [
+        'Content-type: application/x-www-form-urlencoded; charset=utf-8',
+        'accept: application/json; charset=utf-8',
+      ],
+      CURLOPT_POSTFIELDS => http_build_query([
+        'grant_type' => 'client_credentials',
+        'client_id' => $clientIdApiGee,
+        'client_secret' => $ClientSecretApigee
+      ]),
+    ]);
 
-		$curlResponse = curl_exec($curl);
-		$curlHttpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
-		$curlErrorNo = curl_errno($curl);
-		$curlError = curl_error($curl);
+    $curlResponse = curl_exec($curl);
+    $curlHttpCode = curl_getinfo($curl, CURLINFO_HTTP_CODE);
+    $curlErrorNo = curl_errno($curl);
+    $curlError = curl_error($curl);
 
-		curl_close($curl);
-		$finalReq = microtime(true);
-		$executionTime = round($finalReq - $startReq, 2, PHP_ROUND_HALF_UP);
+    curl_close($curl);
+    $finalReq = microtime(true);
+    $executionTime = round($finalReq - $startReq, 2, PHP_ROUND_HALF_UP);
 
-		writeLog('DEBUG', 'RESPONSE IN ' . $executionTime . ' sec CURL HTTP CODE: ' . $curlHttpCode);
+    writeLog('DEBUG', 'RESPONSE IN ' . $executionTime . ' sec CURL HTTP CODE: ' . $curlHttpCode);
 
-		$curlResponse = json_decode($curlResponse);
+    $curlResponse = json_decode($curlResponse);
 
-		if($curlHttpCode !== 200 || !$curlResponse) {
-			switch ($CurlErrorNo) {
-				case 28:
-					$failResponse->rc = 504;
-					break;
-				default:
-					$curlResponse->rc = lang('SETT_RC_DEFAULT');
-			}
+    if ($curlHttpCode !== 200 || !$curlResponse) {
+      switch ($CurlErrorNo) {
+        case 28:
+          $failResponse->rc = 504;
+          break;
+        default:
+          $curlResponse->rc = lang('SETT_RC_DEFAULT');
+      }
 
-			switch ($curlHttpCode) {
-				case 502:
-					$failResponse->rc = 502;
-					break;
-			}
+      switch ($curlHttpCode) {
+        case 502:
+          $failResponse->rc = 502;
+          break;
+      }
+    } else {
+      $curlResponse->rc = 0;
+      $this->CI->session->set_tempdata('jwtOauth', $curlResponse->access_token, 1860);
+    }
 
-		} else {
-			$curlResponse->rc = 0;
-			$this->CI->session->set_tempdata('jwtOauth', $curlResponse->access_token, 1860);
-		}
-
-		return $curlResponse;
-	}
+    return $curlResponse;
+  }
 }
