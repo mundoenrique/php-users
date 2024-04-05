c6f3273c620e007198b91117a3359bf6e7e4c8d8
Enrique Peñaloza <epenaloza@novopayment.com>
2024-04-05 12:22:47 -0400

    Tenant BANORTE bnte

---
+++ build/application/config/config-bnte.php
@@ -0,0 +1,9 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$config['customer'] = 'Mx-Bn';
+$config['customer_uri'] = 'bnt';
+$config['customer_lang'] = $config['customer_uri'];
+$config['customer_style'] = $config['customer_uri'];
+$config['customer_files'] = $config['customer_uri'];
+$config['base_url']	= BASE_URL . '/' . $config['customer_uri'] . '/';
+$config['language']	= BASE_LANGUAGE . '-' . $config['customer_lang'];
