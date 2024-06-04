762a5d72fc3801ecc517d19e7e84f5433898050f
Enrique Peñaloza <epenaloza@novopayment.com>
2024-06-04 09:47:09 -0400

    Adcuaciones migración tomcat

--- build/application/core/NOVO_Controller.php
+++ build/application/core/NOVO_Controller.php
@@ -89,10 +89,10 @@ class NOVO_Controller extends CI_Controller
 
       LoadLangFile('generic', $this->fileLanguage, $this->customerLang);
       clientUrlValidate($this->customerUri);
-      $this->customerUri = $this->config->item('customer_uri');
-      $this->customerLang = $this->config->item('customer_lang');
-      $this->customerStyle = $this->config->item('customer_style');
-      $this->customerFiles = $this->config->item('customer_files');
+      $this->customerUri = config_item('customer_uri');
+      $this->customerLang = config_item('customer_lang');
+      $this->customerStyle = config_item('customer_style');
+      $this->customerFiles = config_item('customer_files');
       LoadLangFile('specific', $this->fileLanguage, $this->customerLang);
 
       if ($this->controllerMethod !== 'suggestion') {
--- build/application/language/global/regexp_bnte_lang.php
+++
@@ -1,6 +0,0 @@
-<?php
-defined('BASEPATH') or exit('No direct script access allowed');
-
-$lang['REGEX_NICKNAME'] = '^([a-z]{2}[a-z0-9_]{4,14})$';
-$lang['REGEX_NICKNAME_PROFILE'] = '^([a-z]{2}[a-z0-9 .,+-_@*]{4,14})$';
-$lang['REGEX_MAXLENGTH_DOC_ID'] = 20;
